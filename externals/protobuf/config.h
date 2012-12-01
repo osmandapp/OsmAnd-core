e8658890627c2cdf8925136090ae03116b4fba1e
Alexey Pelykh <alexey.pelykh@gmail.com>
2012-12-02 00:51:04 +0200

    Externals: protobuf

--- externals/protobuf/config.h
+++ externals/protobuf/config.h
@@ -1,7 +1,33 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
+#ifndef _OSMAND_PROTOBUF_CONFIG_
+#define _OSMAND_PROTOBUF_CONFIG_
 
-#if defined(ANDROID)
+// For WindowsPhone target, InitializeCriticalSection is not supported
+#if defined(_WIN32) && (WINAPI_FAMILY==WINAPI_FAMILY_PHONE_APP)
+#   define InitializeCriticalSection(cs) InitializeCriticalSectionEx((cs), 0, CRITICAL_SECTION_NO_DEBUG_INFO)
+#endif
+
+#if defined(_WIN32) && (WINAPI_FAMILY==WINAPI_FAMILY_PHONE_APP)
+    /* the namespace of hash_map/hash_set */
+	#define HASH_NAMESPACE std
+
+	/* the name of <hash_set> */
+	#define HASH_MAP_CLASS hash_map
+
+	/* the location of <hash_map> */
+	#define HASH_MAP_H <hash_map>
+
+	/* the name of <hash_set> */
+	#define HASH_SET_CLASS hash_set
+
+	/* the location of <hash_set> */
+	#define HASH_SET_H <hash_set>
+
+	/* define if the compiler has hash_map */
+	#define HAVE_HASH_MAP 1
+
+	/* define if the compiler has hash_set */
+	#define HAVE_HASH_SET 1
+#elif defined(ANDROID)
 	/* the namespace of hash_map/hash_set */
 	#define HASH_NAMESPACE std
 
@@ -190,3 +216,5 @@
 #ifndef _TANDEM_SOURCE
 # define _TANDEM_SOURCE 1
 #endif
+
+#endif //  _OSMAND_PROTOBUF_CONFIG_
