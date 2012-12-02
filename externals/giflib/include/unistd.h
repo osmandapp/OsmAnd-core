1126b682a9abe8b0b7270023929b461cdd81038d
Alexey Pelykh <alexey.pelykh@gmail.com>
2012-12-02 12:22:10 +0200

    externals

---
+++ externals/giflib/include/unistd.h
@@ -0,0 +1,10 @@
+#if defined(_WIN32) && (WINAPI_FAMILY==WINAPI_FAMILY_PHONE_APP)
+
+#ifndef _OSMAND_WP_UNISTD_H
+#define _OSMAND_WP_UNISTD_H
+
+// Simply empty file
+
+#endif // _OSMAND_WP_UNISTD_H
+
+#endif // defined(_WIN32) && (WINAPI_FAMILY==WINAPI_FAMILY_PHONE_APP)
