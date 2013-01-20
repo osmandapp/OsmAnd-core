517c302c93d3efb10b68c8d4cfb7692266b2bd92
Victor Shcherb <osmand.app@gmail.com>
2013-01-20 11:46:20 +0100

    Fix local skia

--- targets/linux/OsmAndCore/src/Logging.cpp
+++ targets/linux/OsmAndCore/src/Logging.cpp
@@ -3,10 +3,20 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void OsmAnd::LogPrintf(OsmAnd::LogSeverityLevel level, const char* format, ...)
+extern void OsmAnd::LogPrintf(OsmAnd::LogSeverityLevel level, const char* msg, ...)
 {
 	va_list args;
-	va_start(args, format);
-	//TODO: Do something here
+	va_start( args, msg);
+	if(level == LogSeverityLevel::Error) {
+		printf("ERROR: ");
+	} else if(level == LogSeverityLevel::Info) {
+		printf("INFO: ");
+	} else if(level == LogSeverityLevel::Warning) {
+		printf("WARN: ");
+	} else {
+		printf("DEBUG: ");
+	}
+	vprintf(msg, args);
+	printf("\n");
 	va_end(args);
 }
