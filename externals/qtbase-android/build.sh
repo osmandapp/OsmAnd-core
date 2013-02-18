28d704d78171ab129b4f96727f55f4a9579c4c9d
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-02-18 09:13:16 +0200

    Update qtbase-android scripts

--- externals/qtbase-android/build.sh
+++ externals/qtbase-android/build.sh
@@ -3,25 +3,7 @@
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NAME=$(basename $(dirname "${BASH_SOURCE[0]}") )
 
-export ANDROID_SDK_ROOT=$ANDROID_SDK
-export ANDROID_NDK_ROOT=$ANDROID_NDK
-if [ ! -d "$ANDROID_SDK_ROOT" ]; then
-	echo "ANDROID_SDK_ROOT is not set"
-	exit
-fi
-if [ ! -d "$ANDROID_NDK_ROOT" ]; then
-	echo "ANDROID_NDK_ROOT is not set"
-	exit
-fi
-export ANDROID_NDK_TOOLCHAIN_VERSION=4.7
-export ANDROID_NDK_PLATFORM=android-8
-(cd "$SRCLOC/upstream.patched" && \
-./configure -opensource -confirm-license -xplatform android-g++ \
-	-nomake examples -nomake demos -nomake tests -nomake docs \
-	-qpa \
-	-qt-sql-sqlite \
-	-no-gui -no-widgets -no-opengl -no-accessibility -no-linuxfb -no-directfb -no-eglfs -no-xcb -no-qml-debug -no-javascript-jit \
-	-shared -release \
-	-v && \
-make -j10 \
-)
+(cd "$SRCLOC/upstream.patched.armeabi" && make -j`nproc`)
+(cd "$SRCLOC/upstream.patched.armeabi-v7a" && make -j`nproc`)
+(cd "$SRCLOC/upstream.patched.x86" && make -j`nproc`)
+(cd "$SRCLOC/upstream.patched.mips" && make -j`nproc`)
