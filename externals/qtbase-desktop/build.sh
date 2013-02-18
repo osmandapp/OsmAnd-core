868403841a2febe4a8e36cfe3ed28668b3d85003
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-02-18 22:45:22 +0200

    QtBase for desktop

---
+++ externals/qtbase-desktop/build.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NAME=$(basename $(dirname "${BASH_SOURCE[0]}") )
+
+if [ ! -d "$ANDROID_SDK" ]; then
+	echo "ANDROID_SDK is not set"
+	exit
+fi
+if [ ! -d "$ANDROID_NDK" ]; then
+	echo "ANDROID_NDK is not set"
+	exit
+fi
+export ANDROID_SDK_ROOT=$ANDROID_SDK
+export ANDROID_NDK_ROOT=$ANDROID_NDK
+export ANDROID_NDK_TOOLCHAIN_VERSION=4.7
+
+QTBASE_CONFIGURATION=\
+"-opensource -confirm-license -xplatform android-g++ "\
+"-nomake examples -nomake demos -nomake tests -nomake docs "\
+"-qt-sql-sqlite "\
+"-no-gui -no-widgets -no-opengl -no-accessibility -no-linuxfb -no-directfb -no-eglfs -no-xcb -no-qml-debug -no-javascript-jit "\
+"-c++11 -shared -release "\
+"-v"
+
+if [ ! -d "$SRCLOC/upstream.patched.armeabi" ]; then
+	cp -rf "$SRCLOC/upstream.patched" "$SRCLOC/upstream.patched.armeabi"
+	export ANDROID_TARGET_ARCH=armeabi
+	export ANDROID_NDK_PLATFORM=android-8
+	(cd "$SRCLOC/upstream.patched.armeabi" && \
+		./configure $QTBASE_CONFIGURATION \
+		-no-neon)
+fi
+(cd "$SRCLOC/upstream.patched.armeabi" && make -j`nproc`)
+
+if [ ! -d "$SRCLOC/upstream.patched.armeabi-v7a" ]; then
+	cp -rf "$SRCLOC/upstream.patched" "$SRCLOC/upstream.patched.armeabi-v7a"
+	export ANDROID_TARGET_ARCH=armeabi-v7a
+	export ANDROID_NDK_PLATFORM=android-8
+	(cd "$SRCLOC/upstream.patched.armeabi-v7a" && \
+		./configure $QTBASE_CONFIGURATION \
+		-no-neon)
+fi
+(cd "$SRCLOC/upstream.patched.armeabi-v7a" && make -j`nproc`)
+
+if [ ! -d "$SRCLOC/upstream.patched.armeabi-v7a-neon" ]; then
+	cp -rf "$SRCLOC/upstream.patched" "$SRCLOC/upstream.patched.armeabi-v7a-neon"
+	export ANDROID_TARGET_ARCH=armeabi-v7a
+	export ANDROID_NDK_PLATFORM=android-8
+	(cd "$SRCLOC/upstream.patched.armeabi-v7a-neon" && \
+		./configure $QTBASE_CONFIGURATION \
+		-qtlibinfix _neon)
+fi
+(cd "$SRCLOC/upstream.patched.armeabi-v7a-neon" && make -j`nproc`)
+
+if [ ! -d "$SRCLOC/upstream.patched.x86" ]; then
+	cp -rf "$SRCLOC/upstream.patched" "$SRCLOC/upstream.patched.x86"
+	export ANDROID_TARGET_ARCH=x86
+	export ANDROID_NDK_PLATFORM=android-9
+	(cd "$SRCLOC/upstream.patched.x86" && \
+		./configure $QTBASE_CONFIGURATION)
+fi
+(cd "$SRCLOC/upstream.patched.x86" && make -j`nproc`)
+
+if [ ! -d "$SRCLOC/upstream.patched.mips" ]; then
+	cp -rf "$SRCLOC/upstream.patched" "$SRCLOC/upstream.patched.mips"
+	export ANDROID_TARGET_ARCH=mips
+	export ANDROID_NDK_PLATFORM=android-9
+	(cd "$SRCLOC/upstream.patched.mips" && \
+		./configure $QTBASE_CONFIGURATION)
+fi
+(cd "$SRCLOC/upstream.patched.mips" && make -j`nproc`)
---
+++ externals/qtbase-desktop/configure.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NAME=$(basename $(dirname "${BASH_SOURCE[0]}") )
+
+# Check if tools are present
+if [ ! -f "$SRCLOC/tools.windows.pack" ]; then
+	echo "Downloading '$NAME' tools for Windows..."
+	curl -L https://qt.gitorious.org/qt/qt5/archive-tarball/dev > "$SRCLOC/tools.windows.pack" || { echo "Failure" 1>&2; exit; }
+fi
+if [ ! -d "$SRCLOC/tools.windows" ]; then
+	echo "Extracting '$NAME' tools for Windows..."
+	mkdir -p "$SRCLOC/tools.windows"
+	tar -xf "$SRCLOC/tools.windows.pack" -C "$SRCLOC/tools.windows" "qt-qt5/gnuwin32" --strip=2
+fi
+
+# Check if already configured
+if [ -d "$SRCLOC/upstream.patched" ]; then
+	echo "Skipping external '$NAME': already configured"
+	exit
+fi
+
+# Extract upstream if needed
+if [ ! -d "$SRCLOC/upstream.original" ]; then
+	echo "Downloading '$NAME' upstream..."
+	git clone https://github.com/osmandapp/OsmAnd-external-qtbase.git "$SRCLOC/upstream.original" -b desktop --depth=1
+fi
+
+# Patch
+cp -rf "$SRCLOC/upstream.original" "$SRCLOC/upstream.patched"
+if [ -d "$SRCLOC/patches" ]; then
+	echo "Patching '$NAME'..."
+	PATCHES=`ls -1 $SRCLOC/patches/*.patch | sort`
+	for PATCH in $PATCHES
+	do
+		read  -rd '' PATCH <<< "$PATCH"
+		echo "Applying "`basename $PATCH`
+		patch --strip=1 --directory="$SRCLOC/upstream.patched/" --input="$PATCH"
+	done
+fi
