58c58b4c1611905b4af82191381407929472fef6
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-02-18 09:57:43 +0200

    Another fix for qtbase-android

--- externals/qtbase-android/build.sh
+++ externals/qtbase-android/build.sh
@@ -14,7 +14,6 @@ fi
 export ANDROID_SDK_ROOT=$ANDROID_SDK
 export ANDROID_NDK_ROOT=$ANDROID_NDK
 export ANDROID_NDK_TOOLCHAIN_VERSION=4.7
-export ANDROID_NDK_PLATFORM=android-8
 
 QTBASE_CONFIGURATION=\
 "-opensource -confirm-license -xplatform android-g++ "\
@@ -22,12 +21,13 @@ QTBASE_CONFIGURATION=\
 "-qpa "\
 "-qt-sql-sqlite "\
 "-no-gui -no-widgets -no-opengl -no-accessibility -no-linuxfb -no-directfb -no-eglfs -no-xcb -no-qml-debug -no-javascript-jit "\
-"-shared -release "\
+"-c++11 -shared -release "\
 "-v"
 
 if [ ! -d "$SRCLOC/upstream.patched.armeabi" ]; then
 	cp -rf "$SRCLOC/upstream.patched" "$SRCLOC/upstream.patched.armeabi"
 	export ANDROID_TARGET_ARCH=armeabi
+	export ANDROID_NDK_PLATFORM=android-8
 	(cd "$SRCLOC/upstream.patched.armeabi" && \
 		./configure $QTBASE_CONFIGURATION && \
 		make -j`nproc`)
@@ -36,6 +36,7 @@ fi
 if [ ! -d "$SRCLOC/upstream.patched.armeabi-v7a" ]; then
 	cp -rf "$SRCLOC/upstream.patched" "$SRCLOC/upstream.patched.armeabi-v7a"
 	export ANDROID_TARGET_ARCH=armeabi-v7a
+	export ANDROID_NDK_PLATFORM=android-8
 	(cd "$SRCLOC/upstream.patched.armeabi-v7a" && \
 		./configure $QTBASE_CONFIGURATION && \
 		make -j`nproc`)
@@ -44,6 +45,7 @@ fi
 if [ ! -d "$SRCLOC/upstream.patched.x86" ]; then
 	cp -rf "$SRCLOC/upstream.patched" "$SRCLOC/upstream.patched.x86"
 	export ANDROID_TARGET_ARCH=x86
+	export ANDROID_NDK_PLATFORM=android-9
 	(cd "$SRCLOC/upstream.patched.x86" && \
 		./configure $QTBASE_CONFIGURATION && \
 		make -j`nproc`)
@@ -52,6 +54,7 @@ fi
 if [ ! -d "$SRCLOC/upstream.patched.mips" ]; then
 	cp -rf "$SRCLOC/upstream.patched" "$SRCLOC/upstream.patched.mips"
 	export ANDROID_TARGET_ARCH=mips
+	export ANDROID_NDK_PLATFORM=android-9
 	(cd "$SRCLOC/upstream.patched.mips" && \
 		./configure $QTBASE_CONFIGURATION && \
 		make -j`nproc`)
