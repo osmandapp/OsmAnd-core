2b3bb75384f0d68f92d36111403ed72d117ab8fc
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-02-19 09:20:54 +0200

    Proper windows build

--- externals/qtbase-desktop/build.bat
+++ externals/qtbase-desktop/build.bat
@@ -6,11 +6,12 @@ set PATH=%PATH%;%~dp0\tools.windows\bin
 REM Perform build
 set ORIGINAL_DIR=%CD%
 chdir %~dp0\upstream.patched.windows
-configure.bat ^
+set QTBASE_CONFIGURATION=^
 	-opensource -confirm-license ^
 	-nomake examples -nomake demos -nomake tests -nomake docs ^
 	-qt-sql-sqlite ^
 	-no-widgets -no-opengl -no-openvg -no-accessibility -no-nis ^
-	-c++11 -shared -release
-nmake
+	-c++11 -shared -debug-and-release
+cmd /C "configure.bat %QTBASE_CONFIGURATION%"
+cmd /C "nmake"
 cd %ORIGINAL_DIR%
