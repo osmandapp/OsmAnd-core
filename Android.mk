55a2d81e4d81ab03d1016aa1cc9708e15bf149b7
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-02-26 09:19:58 +0200

    Fix source paths

--- Android.mk
+++ Android.mk
@@ -37,7 +37,7 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_CPP_EXTENSION := .cc .cpp
 LOCAL_SRC_FILES := \
-	$(OSMAND_CORE_RELATIVE)/src/Logging.cpp \
+	$(wildcard $(OSMAND_CORE_RELATIVE)/src/*.c*) \
 	$(OSMAND_CORE_RELATIVE)/native/src/ElapsedTimer.cpp \
 	$(OSMAND_CORE_RELATIVE)/native/src/common.cpp \
 	$(OSMAND_CORE_RELATIVE)/native/src/mapObjects.cpp \
