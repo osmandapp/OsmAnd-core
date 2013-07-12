0969fcc5905569821a315a724663f945adc8c0ff
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-07-12 05:18:52 +0300

    Revert "Remove GDAL - not needed (at least yet)"
    This reverts commit 16d52a062736e76eeb8e8ed333ab2ac23c438125.

---
+++ externals/gdal/cpl_config.h
@@ -0,0 +1,22 @@
+#ifndef _OSMAND_GDAL_CPL_CONFIG_H_
+#define _OSMAND_GDAL_CPL_CONFIG_H_
+
+#if defined(_WIN32)
+
+	#include "upstream.patched/port/cpl_config.h.vc"
+
+#elif defined(ANDROID) || defined(__ANDROID__)
+
+	
+
+#elif defined(__APPLE__)
+
+	
+
+#elif defined(__linux__)
+
+	
+
+#endif
+
+#endif // _OSMAND_GDAL_CPL_CONFIG_H_
