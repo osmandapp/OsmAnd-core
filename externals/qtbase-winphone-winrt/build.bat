02ea0405b4be956b8e037aa78e290915eaf48fc1
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-07-14 13:42:37 +0300

    qt-base for WinPhone and WinRT

---
+++ externals/qtbase-winphone-winrt/build.bat
@@ -0,0 +1,71 @@
+@echo off
+
+REM Prepare environment
+set PATH=%PATH%;%~dp0\tools.windows\bin
+set QTBASE_CONFIGURATION=^
+	-opensource -confirm-license ^
+	-nomake examples -nomake tools ^
+	-qt-sql-sqlite -no-opengl ^
+	-no-widgets -no-accessibility -no-nis ^
+	-no-angle ^
+	-no-style-windowsvista ^
+	-c++11 -shared -debug-and-release
+
+REM Determine target architecture of environment
+for /f "tokens=9 delims= " %%l in ('cl 2^>^&1') do (
+	if "%%l"=="x86" (
+		set envArch=i686
+	)
+	if "%%l"=="x64" (
+		set envArch=amd64
+	)
+	if "%%l"=="ARM" (
+		set envArch=arm
+	)
+	goto envDetected
+)
+:envDetected:
+	
+REM Build for WinPhone(arm and x86) and WinRT(arm, x86 and x64)
+set target=winphone
+set arch=i686
+set xPlatform=winphone-x86-msvc2012
+call :BUILD
+
+set target=winphone
+set arch=arm
+set xPlatform=winphone-arm-msvc2012
+call :BUILD
+
+set target=winrt
+set arch=i686
+set xPlatform=winrt-x86-msvc2012
+call :BUILD
+
+set target=winrt
+set arch=amd64
+set xPlatform=winrt-x64-msvc2012
+call :BUILD
+
+set target=winrt
+set arch=arm
+set xPlatform=winrt-arm-msvc2012
+call :BUILD
+
+goto :EOF
+
+REM ### Function start : BUILD
+:BUILD
+
+REM Check if we have a build directory
+if not exist "%~dp0upstream.patched.%target%.%arch%" (
+	mkdir "%~dp0upstream.patched.%target%.%arch%"
+	xcopy "%~dp0upstream.patched" "%~dp0upstream.patched.%target%.%arch%" /E
+	(pushd %~dp0upstream.patched.%target%.%arch% && (cmd /C "configure.bat %QTBASE_CONFIGURATION% -xplatform %xPlatform%" & popd))
+)
+
+REM Perform build
+(pushd %~dp0upstream.patched.%target%.%arch% && (cmd /C "nmake" & popd))
+
+goto :EOF
+REM ### Function end : BUILD
