12a4fc7f241f93640ddab0a4edd309a46a16479e
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-08-04 09:52:53 +0300

    Test

--- externals/protobuf/Android.mk
+++ externals/protobuf/Android.mk
@@ -12,14 +12,14 @@ endif
 LOCAL_EXPORT_CFLAGS := \
     -DGOOGLE_PROTOBUF_NO_RTTI
 LOCAL_EXPORT_C_INCLUDES := \
-    $(LOCAL_PATH)/upstream.patched/src
+    upstream.patched/src
 
 ifneq ($(OSMAND_USE_PREBUILT),true)
     LOCAL_CFLAGS := \
         $(LOCAL_EXPORT_CFLAGS)
 
     LOCAL_C_INCLUDES := \
-        $(LOCAL_PATH)
+        .
 
     LOCAL_CPP_EXTENSION := .cc
     LOCAL_SRC_FILES := \
