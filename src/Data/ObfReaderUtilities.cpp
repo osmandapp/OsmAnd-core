5e663cae502bf8e69a47b96f53340aa464e37c14
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-09-05 06:49:47 +0300

    Use less space

--- src/Data/ObfReaderUtilities.cpp
+++ src/Data/ObfReaderUtilities.cpp
@@ -79,18 +79,10 @@ void OsmAnd::ObfReaderUtilities::skipUnknownField( gpb::io::CodedInputStream* ci
 
 QString OsmAnd::ObfReaderUtilities::encodeIntegerToString( const uint32_t& value )
 {
-    QChar fakeString[] =
-    {
-        QChar('['),
-        QChar(0x0001 + static_cast<ushort>((value >> 8*0) & 0xff)),
-        QChar(0x0001 + static_cast<ushort>((value >> 8*1) & 0xff)),
-        QChar(0x0001 + static_cast<ushort>((value >> 8*2) & 0xff)),
-        QChar(0x0001 + static_cast<ushort>((value >> 8*3) & 0xff)),
-        QChar(']')
-    };
-    const QString fakeQString(fakeString);
+    QString fakeQString(2, QChar(QChar::Null));
+    fakeQString.data()[0].unicode() = static_cast<ushort>((value >> 16*0) & 0xffff);
+    fakeQString.data()[1].unicode() = static_cast<ushort>((value >> 16*1) & 0xffff);
 
-    assert(fakeQString.length() == 36);
     assert(decodeIntegerFromString(fakeQString) == value);
 
     return fakeQString;
@@ -100,21 +92,13 @@ uint32_t OsmAnd::ObfReaderUtilities::decodeIntegerFromString( const QString& con
 {
     uint32_t res = 0;
 
-    assert(container.length() == 36);
-
     ushort value;
 
-    value = container.at(1 + 0).unicode() - 0x0001;
-    res |= (value & 0xff) << 8*0;
+    value = container.at(0).unicode();
+    res |= (value & 0xffff) << 16*0;
 
-    value = container.at(1 + 1).unicode() - 0x0001;
-    res |= (value & 0xff) << 8*1;
-
-    value = container.at(1 + 2).unicode() - 0x0001;
-    res |= (value & 0xff) << 8*2;
-
-    value = container.at(1 + 3).unicode() - 0x0001;
-    res |= (value & 0xff) << 8*3;
+    value = container.at(1).unicode();
+    res |= (value & 0xffff) << 16*1;
 
     return res;
 }
