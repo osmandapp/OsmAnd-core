029e5239fd4d0a36e033e41af08eb0efaa88830e
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-09-09 08:43:31 +0300

    Update libpng from 1.5.13 to 1.6.3

--- externals/libpng/configure.sh
+++ externals/libpng/configure.sh
@@ -12,7 +12,7 @@ fi
 # Download upstream if needed
 if [ ! -f "$SRCLOC/upstream.pack" ]; then
 	echo "Downloading '$NAME' upstream..."
-	curl -L http://download.osmand.net/prebuilt/libpng-1.5.13.tar.bz2 > "$SRCLOC/upstream.pack" || { echo "Failure" 1>&2; exit; }
+	curl -L http://download.osmand.net/prebuilt/libpng-1.6.3.tar.xz > "$SRCLOC/upstream.pack" || { echo "Failure" 1>&2; exit; }
 fi
 
 # Extract upstream if needed
