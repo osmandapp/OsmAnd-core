7798caf7ac3ef8c45c36ab9b46e573cdd5d4d76d
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-11-21 19:30:15 +0200

    Lost file

---
+++ include/OsmAndCore/stdlib_common.h
@@ -0,0 +1,9 @@
+#ifndef _OSMAND_CORE_STDLIB__COMMON_H_
+#define _OSMAND_CORE_STDLIB__COMMON_H_
+
+#include <cstdint>
+#include <cinttypes>
+#include <memory>
+#include <chrono>
+
+#endif // _OSMAND_CORE_STDLIB__COMMON_H_
