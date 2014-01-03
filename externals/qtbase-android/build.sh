1987ddab9437e3c629f408b2b85eca1cb8c7c4e0
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-01-03 14:44:06 +0200

    Update project settings

--- externals/qtbase-android/build.sh
+++ externals/qtbase-android/build.sh
@@ -14,11 +14,6 @@ if [ ! -d "$ANDROID_NDK" ]; then
 fi
 export ANDROID_SDK_ROOT=`echo $ANDROID_SDK | sed 's/\\\\/\//g'`
 export ANDROID_NDK_ROOT=`echo $ANDROID_NDK | sed 's/\\\\/\//g'`
-if [ -d "$ANDROID_NDK/toolchains/arm-linux-androideabi-4.7" ]; then
-	export ANDROID_NDK_TOOLCHAIN_VERSION=4.7
-elif [ -d "$ANDROID_NDK/toolchains/arm-linux-androideabi-4.8" ]; then
-	export ANDROID_NDK_TOOLCHAIN_VERSION=4.8
-fi
 if [[ "$(uname -a)" =~ Linux ]]; then
 	if [[ "$(uname -m)" == x86_64 ]] && [ -d "$ANDROID_NDK/prebuilt/linux-x86_64" ]; then
 		export ANDROID_NDK_HOST=linux-x86_64;
@@ -47,7 +42,7 @@ if [[ "$(uname -a)" =~ Darwin ]]; then
 fi
 
 QTBASE_CONFIGURATION=$(echo "
-	-xplatform android-g++ -android-toolchain-version $ANDROID_NDK_TOOLCHAIN_VERSION
+	-xplatform android-g++
 	-release -opensource -confirm-license -c++11 -static -largefile -no-accessibility -qt-sql-sqlite
 	-no-javascript-jit -no-qml-debug -qt-zlib -no-gif -no-libpng -no-libjpeg -no-openssl -qt-pcre
 	-nomake examples -nomake tools -no-gui -no-widgets -no-nis -no-cups -no-iconv -no-icu -no-dbus
--- externals/skia/wp8-functions-replacements.h
+++ externals/skia/wp8-functions-replacements.h
@@ -2,10 +2,10 @@
 #define _OSMAND_SKIA_WP8_FUNCTIONS_REPLACEMENTS_H_
 
 #define CreateEvent(lpEventAttributes, bManualReset, bInitialState, lpName) \
-	CreateEventEx((lpEventAttributes), (lpName), ((bManualReset)?CREATE_EVENT_MANUAL_RESET:0)|((bInitialState)?CREATE_EVENT_INITIAL_SET:0), EVENT_MODIFY_STATE)
+    CreateEventEx((lpEventAttributes), (lpName), ((bManualReset)?CREATE_EVENT_MANUAL_RESET:0)|((bInitialState)?CREATE_EVENT_INITIAL_SET:0), EVENT_MODIFY_STATE)
 	
 #define WaitForSingleObject(hHandle, dwMilliseconds) \
-	WaitForSingleObjectEx((hHandle), (dwMilliseconds), FALSE)
+    WaitForSingleObjectEx((hHandle), (dwMilliseconds), FALSE)
 
 #define FindFirstFileW(lpFileName, lpFindFileData) \
     FindFirstFileExW((lpFileName), FindExInfoStandard, (lpFindFileData), FindExSearchNameMatch, NULL, 0)
