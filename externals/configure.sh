98b3c30028688130aa5b30bf326bfd4162f41f9a
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-02-06 19:40:14 +0200

    Add auto-cleaning of externals

--- externals/configure.sh
+++ externals/configure.sh
@@ -7,6 +7,19 @@ if [ -z "$OSMAND_EXTERNALS_SET" ]; then
 	OSMAND_EXTERNALS_SET=*
 fi
 
+last_stamp=""
+if [ -f "$SRCLOC/.stamp" ]; then
+	last_stamp=`cat "$SRCLOC/.stamp"`
+fi
+current_stamp=`cat "$SRCLOC/stamp"`
+echo "Last stamp:    "$last_stamp
+echo "Current stamp: "$current_stamp
+if [ "$last_stamp" != "$current_stamp" ]; then
+	echo "Stamps differ, will clean externals..."
+	"$SRCLOC/clean.sh" $OSMAND_EXTERNALS_SET
+	cp "$SRCLOC/stamp" "$SRCLOC/.stamp"
+fi
+
 for external in ${OSMAND_EXTERNALS_SET[@]/#/$SRCLOC/} ; do
 	if [ -d "$external" ]; then
 		if [ -e "$external/configure.sh" ]; then
