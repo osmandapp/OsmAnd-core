bab2ceed0e42eec9a64a2bba64559428f1df608f
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-02-16 13:55:09 +0200

    Give error if python wasn't found

--- gen-resources-list.sh
+++ gen-resources-list.sh
@@ -15,8 +15,8 @@ if [ ! -f "$PYTHON3" ]; then
 	PYTHON3=`which python3.4`
 fi
 if [ ! -f "$PYTHON3" ]; then
-	echo "Python3 not found"
-	exit
+	echo "Python3 not found in $PATH"
+	exit 1
 fi
 
 "$PYTHON3" "$SRCLOC/gen-resources-list.py" "$@"
