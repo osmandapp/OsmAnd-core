8536b0f3e9bee67dc8a98873bf3cf9c92bfa8efc
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-02-17 20:53:28 +0200

    Fix warnings

--- src/GlobalMemoryManagerOverride.cpp
+++ src/GlobalMemoryManagerOverride.cpp
@@ -1,6 +1,8 @@
 #include "Memory.h"
 
-void* operator new(std::size_t count)
+#include <QtGlobal>
+
+void* operator new(std::size_t count) throw(std::bad_alloc)
 {
     const auto ptr = OsmAnd::getMemoryManager()->allocate(count, "global");
     if(!ptr)
@@ -8,7 +10,7 @@ void* operator new(std::size_t count)
     return ptr;
 }
 
-void* operator new[](std::size_t count)
+void* operator new[](std::size_t count) throw(std::bad_alloc)
 {
     const auto ptr = OsmAnd::getMemoryManager()->allocate(count, "global");
     if(!ptr)
@@ -16,32 +18,32 @@ void* operator new[](std::size_t count)
     return ptr;
 }
 
-void* operator new(std::size_t count, const std::nothrow_t& tag)
+void* operator new(std::size_t count, const std::nothrow_t& tag) Q_DECL_NOTHROW
 {
     return OsmAnd::getMemoryManager()->allocate(count, "global");
 }
 
-void* operator new[](std::size_t count, const std::nothrow_t& tag)
+void* operator new[](std::size_t count, const std::nothrow_t& tag) Q_DECL_NOTHROW
 {
     return OsmAnd::getMemoryManager()->allocate(count, "global");
 }
 
-void operator delete(void* ptr)
+void operator delete(void* ptr) Q_DECL_NOTHROW
 {
     OsmAnd::getMemoryManager()->free(ptr, "global");
 }
 
-void operator delete[](void* ptr)
+void operator delete[](void* ptr) Q_DECL_NOTHROW
 {
     OsmAnd::getMemoryManager()->free(ptr, "global");
 }
 
-void operator delete(void* ptr, const std::nothrow_t& tag)
+void operator delete(void* ptr, const std::nothrow_t& tag) Q_DECL_NOTHROW
 {
     OsmAnd::getMemoryManager()->free(ptr, "global");
 }
 
-void operator delete[](void* ptr, const std::nothrow_t& tag)
+void operator delete[](void* ptr, const std::nothrow_t& tag) Q_DECL_NOTHROW
 {
     OsmAnd::getMemoryManager()->free(ptr, "global");
 }
