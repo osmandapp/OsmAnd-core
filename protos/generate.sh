2d6cad8a43655039f8f0921e01edaff4e83763b8
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-02-24 13:16:45 +0200

    Use OBF format from resources

---
+++ protos/generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Switch to default protoc if not provided
+if [ -z "$PROTOC" ]; then
+    PROTOC="protoc"
+fi
+if [ -z "$PROTOC" ]; then
+	echo "'protoc' not found, terminating"
+	exit 1
+fi
+echo "Using $PROTOC"
+
+"$PROTOC" --cpp_out="$SRCLOC" --proto_path="$SRCLOC/../../resources/protos" "$SRCLOC/../../resources/protos/OBF.proto"
