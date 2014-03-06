606217c97020a1d9f96757f96d2bc2e0fa649279
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-03-06 20:12:45 +0100

    Update

--- wrappers/android/build.sh
+++ wrappers/android/build.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
+if [ -z "$BASH_VERSION" ]; then
+	exec bash "$0" "$@"
+	exit $?
+fi
+
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REAL_SRCLOC=`readlink -f $SRCLOC`
 
 OSMAND_CORE_ROOT_RELATIVE="../.."
 OSMAND_CORE_ROOT="$REAL_SRCLOC/$OSMAND_CORE_ROOT_RELATIVE"
 
-if [[ "$(uname -a)" =~ Linux ]]; then
+HOST=`uname -a`
+if [[ "$HOST" =~ "Linux" ]]; then
 	if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
 		OSMAND_BUILD_CPU_CORES_NUM=`nproc`
 	fi
-elif [[ "$(uname -a)" =~ Darwin ]]; then
+elif [[ "$HOST" =~ "Darwin" ]]; then
 	if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
 		OSMAND_BUILD_CPU_CORES_NUM=`sysctl hw.ncpu | awk '{print $2}'`
 	fi
-elif [[ "$(uname -a)" =~ Cygwin ]]; then
+elif [[ "$HOST" =~ "Cygwin" ]]; then
 	if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
 		OSMAND_BUILD_CPU_CORES_NUM=`nproc`
 	fi
 else
-	OSMAND_BUILD_CPU_CORES_NUM=
+	echo "no"
+	if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
+		OSMAND_BUILD_CPU_CORES_NUM=""
+	fi
 fi
 
 export OSMAND_CORE_ROOT
@@ -28,4 +37,5 @@ echo "Using OsmAnd Core root: $OSMAND_CORE_ROOT"
 echo "Using OsmAnd Core root (relative): $OSMAND_CORE_ROOT_RELATIVE"
 
 echo "Will execute from $REAL_SRCLOC"
+echo "Using $OSMAND_BUILD_CPU_CORES_NUM core(s)"
 (cd $REAL_SRCLOC && ndk-build -j$OSMAND_BUILD_CPU_CORES_NUM)
