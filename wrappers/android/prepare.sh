0757cc186775e5aadabf033182ce777a91686525
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-03-07 02:46:18 +0200

    Preparation script

---
+++ wrappers/android/prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$BASH_VERSION" ]; then
+	exec bash "$0" "$@"
+	exit $?
+fi
+
+# Get root
+SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT="$SRCLOC/../../.."
+
+# Prepare core dependencies
+echo "Configuring dependencies..."
+"$ROOT/core/externals/configure.sh" expat freetype gdal giflib glm glsl-optimizer jpeg libpng protobuf qtbase-android skia icu4c boost-android
+echo "Building dependencies..."
+"$ROOT/core/externals/build.sh"
