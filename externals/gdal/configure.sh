34ffef593b5f8932138f7b48392af6b2add852c9
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-03-11 23:02:44 +0200

    Update GDAL

--- externals/gdal/configure.sh
+++ externals/gdal/configure.sh
@@ -8,6 +8,22 @@ fi
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NAME=$(basename $SRCLOC)
 
+# Check if needs reconfiguring
+if [ -f "$SRCLOC/stamp" ]; then
+	last_stamp=""
+	if [ -f "$SRCLOC/.stamp" ]; then
+		last_stamp=`cat "$SRCLOC/.stamp"`
+	fi
+	current_stamp=`cat "$SRCLOC/stamp"`
+	echo "Last stamp:    "$last_stamp
+	echo "Current stamp: "$current_stamp
+	if [ "$last_stamp" != "$current_stamp" ]; then
+		echo "Stamps differ, will clean external '$NAME'..."
+		"$SRCLOC/../clean.sh" $NAME
+		cp "$SRCLOC/stamp" "$SRCLOC/.stamp"
+	fi
+fi
+
 # Check if already configured
 if [ -d "$SRCLOC/upstream.patched" ]; then
 	echo "Skipping external '$NAME': already configured"
@@ -17,7 +33,7 @@ fi
 # Download upstream if needed
 if [ ! -f "$SRCLOC/upstream.pack" ]; then
 	echo "Downloading '$NAME' upstream..."
-	curl -L http://download.osgeo.org/gdal/1.10.0/gdal-1.10.0.tar.gz > "$SRCLOC/upstream.pack" || { echo "Failure" 1>&2; exit; }
+	curl -L http://download.osgeo.org/gdal/1.10.1/gdal-1.10.1.tar.gz > "$SRCLOC/upstream.pack" || { echo "Failure" 1>&2; exit; }
 fi
 
 # Extract upstream if needed
