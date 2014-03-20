61ea735ae69430d9f0d9cafac4258782d5c11696
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-03-20 14:20:22 +0200

    Updated GLM to 0.9.5.2

--- externals/glm/configure.sh
+++ externals/glm/configure.sh
@@ -8,6 +8,22 @@ fi
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NAME=$(basename $SRCLOC)
 
+# Check if needs reconfiguring
+if [ -f "$SRCLOC/stamp" ]; then
+	last_stamp=""
+	if [ -f "$SRCLOC/.stamp" ]; then
+		last_stamp=`cat "$SRCLOC/.stamp"`
+	fi
+	current_stamp=`cat "$SRCLOC/stamp"`
+	echo "Last stamp:    "$last_stamp
+	echo "Current stamp: "$current_stamp
+	if [ "$last_stamp" != "$current_stamp" ]; then
+		echo "Stamps differ, will clean external '$NAME'..."
+		"$SRCLOC/../clean.sh" $NAME
+		cp "$SRCLOC/stamp" "$SRCLOC/.stamp"
+	fi
+fi
+
 # Check if already configured
 if [ -d "$SRCLOC/upstream.patched" ]; then
 	echo "Skipping external '$NAME': already configured"
@@ -17,7 +33,7 @@ fi
 # Download upstream if needed
 if [ ! -f "$SRCLOC/upstream.pack" ]; then
 	echo "Downloading '$NAME' upstream..."
-	curl -L http://sourceforge.net/projects/ogl-math/files/glm-0.9.4.4/glm-0.9.4.4.zip/download > "$SRCLOC/upstream.pack" || { echo "Failure" 1>&2; exit; }
+	curl -L http://sourceforge.net/projects/ogl-math/files/glm-0.9.5.2/glm-0.9.5.2.zip/download > "$SRCLOC/upstream.pack" || { echo "Failure" 1>&2; exit; }
 fi
 
 # Extract upstream if needed
@@ -25,7 +41,7 @@ if [ ! -d "$SRCLOC/upstream.original" ]; then
 	echo "Extracting '$NAME' upstream..."
 	mkdir -p "$SRCLOC/upstream.tmp"
 	unzip -q "$SRCLOC/upstream.pack" -d "$SRCLOC/upstream.tmp"
-	mv "$SRCLOC/upstream.tmp/glm-"* "$SRCLOC/upstream.original"
+	mv "$SRCLOC/upstream.tmp/glm" "$SRCLOC/upstream.original"
 	rm -rf "$SRCLOC/upstream.tmp"
 fi
 
