3f372e33b56ffdd3443b19b2eed39e6149fc29ce
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-07-02 15:02:13 +0300

    Fix cmake warnings

--- wrappers/wrappers.cmake
+++ wrappers/wrappers.cmake
@@ -2,5 +2,4 @@
 if(CMAKE_SHARED_LIBS_ALLOWED_ON_TARGET AND (CMAKE_TARGET_OS STREQUAL "linux" OR CMAKE_TARGET_OS STREQUAL "darwin" OR CMAKE_TARGET_OS STREQUAL "windows" OR CMAKE_TARGET_OS STREQUAL "android"))
 	# JNI wrapper is only valid on platforms where Java exists
 	add_subdirectory("${OSMAND_ROOT}/core/wrappers/java" "OsmAndCore_Java")
-	add_dependencies(OsmAndCoreJNI OsmAndCore)
 endif()
