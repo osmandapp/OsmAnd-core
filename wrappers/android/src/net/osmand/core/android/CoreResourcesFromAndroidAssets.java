3b490c8cfe075cecb5c9f3f58f356682d2be6bef
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-09-07 11:54:07 +0300

    Rename

--- wrappers/android/src/net/osmand/core/android/CoreResourcesFromAndroidAssets.java
+++ wrappers/android/src/net/osmand/core/android/CoreResourcesFromAndroidAssets.java
@@ -1,8 +1,18 @@
 package net.osmand.core.android;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+
 import net.osmand.core.jni.ICoreResourcesProvider;
 
 public class CoreResourcesFromAndroidAssets extends ICoreResourcesProvider {
+    public CoreResourcesFromAndroidAssets(final Context context) {
+        _context = context;
+    }
+
+    private final Context _context;
+
     @Override
     public SWIGTYPE_p_QByteArray getResource(String name, float displayDensityFactor, SWIGTYPE_p_bool ok) {
         return null;
--- wrappers/android/src/net/osmand/core/android/CoreResourcesFromAndroid_LegacyOsmAndCompatible.java
+++ wrappers/android/src/net/osmand/core/android/CoreResourcesFromAndroid_LegacyOsmAndCompatible.java
@@ -1,8 +1,18 @@
 package net.osmand.core.android;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+
 import net.osmand.core.jni.ICoreResourcesProvider;
 
-public class CoreResourcesFromAndroidAssetsAndResources_Legacy extends ICoreResourcesProvider {
+public class CoreResourcesFromAndroid_LegacyOsmAndCompatible extends ICoreResourcesProvider {
+    public CoreResourcesFromAndroid_LegacyOsmAndCompatible(final Context context) {
+        _context = context;
+    }
+
+    private final Context _context;
+
     @Override
     public SWIGTYPE_p_QByteArray getResource(String name, float displayDensityFactor, SWIGTYPE_p_bool ok) {
         return null;
