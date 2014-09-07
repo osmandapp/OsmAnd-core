9edb0ca3ea902a6334001c83acd783ada00fbf93
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-09-07 21:25:43 +0300

    Update CoreResourcesFromAndroidAssets.java

--- wrappers/android/src/net/osmand/core/android/CoreResourcesFromAndroidAssets.java
+++ wrappers/android/src/net/osmand/core/android/CoreResourcesFromAndroidAssets.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.HashMap;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.util.Log;
