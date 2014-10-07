c20df74d9fe5738e25f09d314f734986d45ab901
Alexey Pelykh <alexey.pelykh@gmail.com>
2014-10-07 14:13:24 +0300

    Fix - disable

--- include/OsmAndCore/Map/IMapRasterBitmapTileProvider.h
+++ include/OsmAndCore/Map/IMapRasterBitmapTileProvider.h
@@ -29,28 +29,28 @@ namespace OsmAnd
         virtual float getTileDensityFactor() const = 0;
     };
 	
-    SWIG_EMIT_DIRECTOR_BEGIN(IMapRasterBitmapTileProvider);
-        SWIG_EMIT_DIRECTOR_CONST_METHOD_NO_ARGS(
-            ZoomLevel,
-            getMinZoom);
-        SWIG_EMIT_DIRECTOR_CONST_METHOD_NO_ARGS(
-            ZoomLevel,
-            getMaxZoom);
-        SWIG_EMIT_DIRECTOR_METHOD(
-            bool,
-            obtainData,
-            /*SWIG_OMIT(const)*/ TileId tileId,
-            const ZoomLevel zoom,
-            std::shared_ptr<MapTiledData>& outTiledData,
-            const IQueryController* const queryController);
-        SWIG_EMIT_SHARED_PTR_REFERENCE_ASSIGN(MapTiledData);
-        SWIG_EMIT_DIRECTOR_CONST_METHOD_NO_ARGS(
-            uint32_t,
-            getTileSize);
-        SWIG_EMIT_DIRECTOR_CONST_METHOD_NO_ARGS(
-            float,
-            getTileDensityFactor);
-    SWIG_EMIT_DIRECTOR_END(IMapRasterBitmapTileProvider);
+//    SWIG_EMIT_DIRECTOR_BEGIN(IMapRasterBitmapTileProvider);
+//        SWIG_EMIT_DIRECTOR_CONST_METHOD_NO_ARGS(
+//            ZoomLevel,
+//            getMinZoom);
+//        SWIG_EMIT_DIRECTOR_CONST_METHOD_NO_ARGS(
+//            ZoomLevel,
+//            getMaxZoom);
+//        SWIG_EMIT_DIRECTOR_METHOD(
+//            bool,
+//            obtainData,
+//            /*SWIG_OMIT(const)*/ TileId tileId,
+//            const ZoomLevel zoom,
+//            std::shared_ptr<MapTiledData>& outTiledData,
+//            const IQueryController* const queryController);
+//        SWIG_EMIT_SHARED_PTR_REFERENCE_ASSIGN(MapTiledData);
+//        SWIG_EMIT_DIRECTOR_CONST_METHOD_NO_ARGS(
+//            uint32_t,
+//            getTileSize);
+//        SWIG_EMIT_DIRECTOR_CONST_METHOD_NO_ARGS(
+//            float,
+//            getTileDensityFactor);
+//    SWIG_EMIT_DIRECTOR_END(IMapRasterBitmapTileProvider);
 
     class OSMAND_CORE_API RasterBitmapTile : public MapTiledData
     {
