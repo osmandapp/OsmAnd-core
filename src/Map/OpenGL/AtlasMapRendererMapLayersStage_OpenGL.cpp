18ac56874175fa991456e034a8a48bce49aef73b
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-02-13 10:33:33 +0200

    Test

--- src/Map/OpenGL/AtlasMapRendererMapLayersStage_OpenGL.cpp
+++ src/Map/OpenGL/AtlasMapRendererMapLayersStage_OpenGL.cpp
@@ -367,6 +367,9 @@ bool OsmAnd::AtlasMapRendererMapLayersStage_OpenGL::initializeRasterLayersProgra
         "        v2f_texCoordsPerLayer_0);                                                                                  ""\n"
         "#endif // TEXTURE_LOD_SUPPORTED                                                                                    ""\n"
         "    addExtraAlpha(finalColor, param_fs_rasterTileLayer_0.opacity, param_fs_rasterTileLayer_0.isPremultipliedAlpha);""\n"
+        //////////////////////////////////////////////////////////////////////////
+        "    lowp vec4 TRUE_COLOR = finalColor;                                                                                          ""\n"
+        //////////////////////////////////////////////////////////////////////////
         "    lowp float firstLayerColorFactor = param_fs_rasterTileLayer_0.isPremultipliedAlpha +                           ""\n"
         "        (1.0 - param_fs_rasterTileLayer_0.isPremultipliedAlpha) * finalColor.a;                                    ""\n"
         "    finalColor *= vec4(firstLayerColorFactor, firstLayerColorFactor, firstLayerColorFactor, 1.0);                  ""\n"
@@ -389,6 +392,9 @@ bool OsmAnd::AtlasMapRendererMapLayersStage_OpenGL::initializeRasterLayersProgra
         "    }                                                                                                              ""\n"
 #endif
         "    FRAGMENT_COLOR_OUTPUT = finalColor;                                                                            ""\n"
+        //////////////////////////////////////////////////////////////////////////
+        "    FRAGMENT_COLOR_OUTPUT.arg = TRUE_COLOR.arg;                                                                            ""\n"
+        //////////////////////////////////////////////////////////////////////////
         "}                                                                                                                  ""\n");
     const auto& fragmentShader_perRasterLayer = QString::fromLatin1(
         "    {                                                                                                              ""\n"
