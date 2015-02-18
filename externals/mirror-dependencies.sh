491b3b8d1089f27ce2f47868345ab01d2c349e9c
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-02-18 11:57:18 +0200

    Fix

--- externals/mirror-dependencies.sh
+++ externals/mirror-dependencies.sh
@@ -6,6 +6,20 @@ if [ -z "$BASH_VERSION" ]; then
 	exit $?
 fi
 
+# Function: download(from, to)
+function download
+{
+	local from=$1
+	local to=$2
+	curl --fail "$from" > "$to"
+	
+	local exitCode=$?
+	if [ $exitCode -ne 0 ]; then
+		echo "Download $from failed"
+		exit $exitCode
+	fi
+}
+
 # Should point to local path, exposed to public as http://builder.osmand.net/dependencies-mirror/
 if [ -z "$DEPENDENCIES_MIRROR" ]; then
 	DEPENDENCIES_MIRROR=$1
@@ -15,86 +29,15 @@ if [ -z "$DEPENDENCIES_MIRROR" ]; then
 	exit 1
 fi
 
-curl http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.bz2/download > "$DEPENDENCIES_MIRROR/boost_1_56_0.tar.bz2"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl http://sourceforge.net/projects/expat/files/expat/2.1.0/expat-2.1.0.tar.gz/download > "$DEPENDENCIES_MIRROR/expat-2.1.0.tar.gz"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl http://sourceforge.net/projects/freetype/files/freetype2/2.5.0/freetype-2.5.0.1.tar.bz2/download > "$DEPENDENCIES_MIRROR/freetype-2.5.0.1.tar.bz2"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz > "$DEPENDENCIES_MIRROR/gdal-1.11.1.tar.gz"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl https://sourceforge.net/projects/glew/files/glew/1.11.0/glew-1.11.0.tgz/download > "$DEPENDENCIES_MIRROR/glew-1.11.0.tgz"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl http://sourceforge.net/projects/ogl-math/files/glm-0.9.5.3/glm-0.9.5.3.zip/download > "$DEPENDENCIES_MIRROR/glm-0.9.5.3.zip"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl http://download.icu-project.org/files/icu4c/52.1/icu4c-52_1-src.tgz > "$DEPENDENCIES_MIRROR/icu4c-52_1-src.tgz"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl http://www.ijg.org/files/jpegsrc.v9.tar.gz > "$DEPENDENCIES_MIRROR/jpegsrc.v9.tar.gz"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl http://www.libarchive.org/downloads/libarchive-3.1.2.tar.gz > "$DEPENDENCIES_MIRROR/libarchive-3.1.2.tar.gz"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl http://sourceforge.net/projects/libpng/files/libpng16/older-releases/1.6.6/libpng-1.6.6.tar.xz/download > "$DEPENDENCIES_MIRROR/libpng-1.6.6.tar.xz"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.bz2 > "$DEPENDENCIES_MIRROR/protobuf-2.4.1.tar.bz2"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
-
-curl http://zlib.net/zlib-1.2.8.tar.gz > "$DEPENDENCIES_MIRROR/zlib-1.2.8.tar.gz"
-exitCode=$?
-if [ $exitCode -ne 0 ]; then
-	echo "Mirroring failed!"
-	exit $exitCode
-fi
+download "http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.bz2/download" "$DEPENDENCIES_MIRROR/boost_1_56_0.tar.bz2"
+download "http://sourceforge.net/projects/expat/files/expat/2.1.0/expat-2.1.0.tar.gz/download" "$DEPENDENCIES_MIRROR/expat-2.1.0.tar.gz"
+download "http://sourceforge.net/projects/freetype/files/freetype2/2.5.0/freetype-2.5.0.1.tar.bz2/download" "$DEPENDENCIES_MIRROR/freetype-2.5.0.1.tar.bz2"
+download "http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz" "$DEPENDENCIES_MIRROR/gdal-1.11.1.tar.gz"
+download "http://sourceforge.net/projects/glew/files/glew/1.11.0/glew-1.11.0.tgz/download" "$DEPENDENCIES_MIRROR/glew-1.11.0.tgz"
+download "http://sourceforge.net/projects/ogl-math/files/glm-0.9.5.3/glm-0.9.5.3.zip/download" "$DEPENDENCIES_MIRROR/glm-0.9.5.3.zip"
+download "http://download.icu-project.org/files/icu4c/52.1/icu4c-52_1-src.tgz" "$DEPENDENCIES_MIRROR/icu4c-52_1-src.tgz"
+download "http://www.ijg.org/files/jpegsrc.v9.tar.gz" "$DEPENDENCIES_MIRROR/jpegsrc.v9.tar.gz"
+download "http://www.libarchive.org/downloads/libarchive-3.1.2.tar.gz" "$DEPENDENCIES_MIRROR/libarchive-3.1.2.tar.gz"
+download "http://sourceforge.net/projects/libpng/files/libpng16/older-releases/1.6.6/libpng-1.6.6.tar.xz/download" "$DEPENDENCIES_MIRROR/libpng-1.6.6.tar.xz"
+download "http://protobuf.googlecode.com/files/protobuf-2.4.1.tar.bz2" "$DEPENDENCIES_MIRROR/protobuf-2.4.1.tar.bz2"
+download "http://zlib.net/zlib-1.2.8.tar.gz" "$DEPENDENCIES_MIRROR/zlib-1.2.8.tar.gz"
