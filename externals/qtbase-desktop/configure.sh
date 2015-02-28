5bb1c400ea83952aef603b491a07b6f13672f272
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-02-28 15:29:59 +0200

    Update Qt to 5.4.1 for desktop

--- externals/qtbase-desktop/configure.sh
+++ externals/qtbase-desktop/configure.sh
@@ -8,7 +8,7 @@ fi
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SRCLOC/../../../build/utils/functions.sh"
 
-prepareUpstreamFromGit "$SRCLOC" "https://github.com/osmandapp/OsmAnd-external-qtbase.git" "qt-v5.3.0"
+prepareUpstreamFromGit "$SRCLOC" "https://github.com/osmandapp/OsmAnd-external-qtbase.git" "qt-v5.4.1"
 cp -rpf "$SRCLOC/upstream.original/mkspecs/macx-clang" "$SRCLOC/upstream.original/mkspecs/macx-clang-libc++-32"
 cp -rpf "$SRCLOC/upstream.original/mkspecs/macx-clang" "$SRCLOC/upstream.original/mkspecs/macx-clang-libc++-64"
 cp -rpf "$SRCLOC/upstream.original/mkspecs/linux-clang" "$SRCLOC/upstream.original/mkspecs/linux-clang-32"
