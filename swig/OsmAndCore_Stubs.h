c43c1a2e918a9c1646126949ebe3e923f2303640
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-04-06 19:25:06 +0300

    Fix

--- swig/OsmAndCore_Stubs.h
+++ swig/OsmAndCore_Stubs.h
@@ -1,6 +1,7 @@
 #define OSMAND_CORE_API
 #define Q_DECL_CONSTEXPR
 #define Q_DECL_FINAL
+#define Q_DECL_OVERRIDE
 #define Q_DISABLE_COPY_AND_MOVE(x) %nocopyctor x;
 #define Q_DISABLE_MOVE(x) %nocopyctor x;
 #define Q_DISABLE_COPY(x) %nocopyctor x;
