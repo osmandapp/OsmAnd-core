23b202faf757c2005005f68a464be280bc28922f
Alexey Kulish <alexey.kulish@osmand.net>
2016-06-10 23:45:52 +0300

    Added raw code for search by name

--- samples/android/sample1/src/net/osmand/core/samples/android/sample1/MainActivity.java
+++ samples/android/sample1/src/net/osmand/core/samples/android/sample1/MainActivity.java
@@ -1,13 +1,42 @@
 package net.osmand.core.samples.android.sample1;
 
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 
-import net.osmand.core.jni.*;
-import net.osmand.core.android.*;
+import net.osmand.core.android.AtlasMapRendererView;
+import net.osmand.core.android.CoreResourcesFromAndroidAssets;
+import net.osmand.core.android.NativeCore;
+import net.osmand.core.jni.AmenitiesByNameSearch;
+import net.osmand.core.jni.Amenity;
+import net.osmand.core.jni.AreaI;
+import net.osmand.core.jni.IMapLayerProvider;
+import net.osmand.core.jni.IMapStylesCollection;
+import net.osmand.core.jni.IObfsCollection;
+import net.osmand.core.jni.IQueryController;
+import net.osmand.core.jni.ISearch;
+import net.osmand.core.jni.LatLon;
+import net.osmand.core.jni.LogSeverityLevel;
+import net.osmand.core.jni.Logger;
+import net.osmand.core.jni.MapObjectsSymbolsProvider;
+import net.osmand.core.jni.MapPresentationEnvironment;
+import net.osmand.core.jni.MapPrimitivesProvider;
+import net.osmand.core.jni.MapPrimitiviser;
+import net.osmand.core.jni.MapRasterLayerProvider_Software;
+import net.osmand.core.jni.MapRendererStateChanges;
+import net.osmand.core.jni.MapStylesCollection;
+import net.osmand.core.jni.ObfDataType;
+import net.osmand.core.jni.ObfDataTypesMask;
+import net.osmand.core.jni.ObfInfo;
+import net.osmand.core.jni.ObfMapObjectsProvider;
+import net.osmand.core.jni.ObfsCollection;
+import net.osmand.core.jni.PointI;
+import net.osmand.core.jni.QIODeviceLogSink;
+import net.osmand.core.jni.ResolvedMapStyle;
+import net.osmand.core.jni.Utilities;
+import net.osmand.core.jni.ZoomLevel;
 
 public class MainActivity extends ActionBarActivity {
     private static final String TAG = "OsmAndCoreSample";
@@ -27,9 +56,14 @@ public class MainActivity extends ActionBarActivity {
     private IMapLayerProvider _mapLayerProvider1;
     private QIODeviceLogSink _fileLogSink;
 
+	private final static int MAX_RESULTS = 10;
+	private int resCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        long startTime = System.currentTimeMillis();
 
         // Initialize native core prior (if needed)
         if (NativeCore.isAvailable() && !NativeCore.isLoaded())
@@ -105,7 +139,58 @@ public class MainActivity extends ActionBarActivity {
 
         _mapLayerProvider0 = new MapRasterLayerProvider_Software(_mapPrimitivesProvider);
         _mapView.setMapLayerProvider(0, _mapLayerProvider0);
-    }
+
+		Log.d(TAG, "NATIVE_INITIALIZED = " + (System.currentTimeMillis() - startTime) / 1000f);
+
+		System.out.println("=== Start search");
+		resCount = 0;
+
+		AmenitiesByNameSearch byNameSearch = new AmenitiesByNameSearch(_obfsCollection);
+		AmenitiesByNameSearch.Criteria criteria = new AmenitiesByNameSearch.Criteria();
+		criteria.setName("bank");
+
+		_obfsCollection.obtainDataInterface();
+
+		IObfsCollection.IAcceptorFunction acceptorFunction = new IObfsCollection.IAcceptorFunction() {
+			@Override
+			public boolean method(ObfInfo obfInfo) {
+				// Amsterdam
+				PointI topLeftPoint = Utilities.convertLatLonTo31(new LatLon(52.444457, 4.757098));
+				PointI bottomRightPoint = Utilities.convertLatLonTo31(new LatLon(52.322610,4.975451));
+				AreaI visibleArea = new AreaI(topLeftPoint, bottomRightPoint);
+
+
+				ObfDataTypesMask mask = new ObfDataTypesMask();
+				mask.set(ObfDataType.POI);
+
+				//boolean res = obfInfo.containsDataFor(visibleArea, ZoomLevel.MinZoomLevel, ZoomLevel.MaxZoomLevel, mask);
+				return true;//res;
+			}
+		};
+
+		criteria.setSourceFilter(acceptorFunction.getBinding());
+
+		ISearch.INewResultEntryCallback newResultEntryCallback = new ISearch.INewResultEntryCallback() {
+			@Override
+			public void method(ISearch.Criteria criteria, ISearch.IResultEntry resultEntry) {
+				ResultEntry res = new ResultEntry(resultEntry);
+				Amenity amenity = res.getAmenity();
+				System.out.println("Poi found === " + amenity.getNativeName());
+				amenity.getLocalizedNames();
+				resCount++;
+			}
+		};
+
+		byNameSearch.performSearch(criteria, newResultEntryCallback.getBinding(), new IQueryController() {
+			@Override
+			public boolean isAborted() {
+				return resCount >= MAX_RESULTS;
+			}
+		});
+
+		System.out.println("=== Finish search");
+
+	}
 
     private AtlasMapRendererView _mapView;
 
@@ -129,4 +214,10 @@ public class MainActivity extends ActionBarActivity {
 
         super.onDestroy();
     }
+
+	private static class ResultEntry extends AmenitiesByNameSearch.ResultEntry {
+		protected ResultEntry(ISearch.IResultEntry resultEntry) {
+			super(ISearch.IResultEntry.getCPtr(resultEntry), false);
+		}
+	}
 }
