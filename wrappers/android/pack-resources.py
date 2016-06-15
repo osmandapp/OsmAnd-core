81a83beba38b9a745324c056a93beaee0b3a39f3
Roman Inflianskas <infroma@gmail.com>
2016-06-15 18:03:50 +0300

    No qz for png files

--- wrappers/android/pack-resources.py
+++ wrappers/android/pack-resources.py
@@ -17,19 +17,26 @@ class OsmAndCoreResourcesPacker(object):
 
     # -------------------------------------------------------------------------
     def pack(self, workroot):
-        # Pack each file found in working directory (except for starting with dot)
+        # Pack each file found in working directory
         for path, dirs, files in os.walk(workroot):
-            if path.startswith('.'):
+            def is_path_ignored(path):
+                return path.startswith('.') or path.endswith('.png')
+
+            if is_path_ignored(path):
                 print("Ignoring '%s'" % (path))
                 continue
             print("Listing '%s' with %d files" % (path, len(files)))
             for filename in files:
+                if is_path_ignored(filename):
+                    print("Ignoring '%s'" % (filename))
+                    continue
+
                 filepath = os.path.join(path, filename)
 
                 originalSize = os.path.getsize(filepath)
                 with open(filepath, "rb") as inputFile:
                     fileContent = inputFile.read()
-                
+
                 packedContent = zlib.compress(fileContent, 9)
                 packedSize = len(packedContent)
 
