eeeb436cc2238190e20324b1a2ee6ae8d54a395b
Roman Inflianskas <infroma@gmail.com>
2016-07-03 23:06:15 +0300

    Reverse geocoding: PIMPL

--- include/OsmAndCore/Search/ReverseGeocoder.h
+++ include/OsmAndCore/Search/ReverseGeocoder.h
@@ -1,5 +1,5 @@
-#ifndef REVERSEGEOCODER_H
-#define REVERSEGEOCODER_H
+#ifndef _OSMAND_CORE_REVERSE_GEOCODER_H_
+#define _OSMAND_CORE_REVERSE_GEOCODER_H_
 
 #include <OsmAndCore/stdlib_common.h>
 
@@ -11,21 +11,25 @@
 #include <OsmAndCore/restore_internal_warnings.h>
 
 #include <OsmAndCore.h>
+#include <OsmAndCore/PrivateImplementation.h>
+#include <OsmAndCore/Data/Building.h>
 #include <OsmAndCore/Data/StreetGroup.h>
 #include <OsmAndCore/Data/Street.h>
-#include <OsmAndCore/IObfsCollection.h>
+//#include <OsmAndCore/IObfsCollection.h>
 #include <OsmAndCore/IRoadLocator.h>
 #include <OsmAndCore/LatLon.h>
 #include <OsmAndCore/Nullable.h>
 #include <OsmAndCore/PointsAndAreas.h>
-#include <OsmAndCore/Search/AddressesByNameSearch.h>
 #include <OsmAndCore/Search/BaseSearch.h>
 
 namespace OsmAnd
 {
+    class ReverseGeocoder_P;
     class OSMAND_CORE_API ReverseGeocoder Q_DECL_FINAL : public BaseSearch
     {
         Q_DISABLE_COPY_AND_MOVE(ReverseGeocoder)
+    private:
+        PrivateImplementation<ReverseGeocoder_P> _p;
     public:
         struct OSMAND_CORE_API Criteria : public BaseSearch::Criteria
         {
@@ -36,11 +40,11 @@ namespace OsmAnd
         struct OSMAND_CORE_API ResultEntry : public IResultEntry
         {
         public:
+            ResultEntry();
+
             Nullable<LatLon> searchPoint;
 
             Nullable<LatLon> connectionPoint;
-//            int regionFP;
-//            int regionLen;
 //            Nullable<const LatLon>& point;  // RouteSegmentPoint in Java
             QString streetName;
 
@@ -55,15 +59,6 @@ namespace OsmAnd
             void setDistance(double dist);
             QString toString() const;
 
-//            inline double getDistanceP() {
-//                if (point != null && searchPoint != null) {
-//                    // Need distance between searchPoint and nearest RouteSegmentPoint here, to approximate distance from nearest named road
-//                    return Math.sqrt(point.distSquare);
-//                } else {
-//                    return NAN;
-//                }
-//            }
-
 //        private:
             double dist = NAN;
         };
@@ -71,29 +66,13 @@ namespace OsmAnd
         explicit ReverseGeocoder(
                 const std::shared_ptr<const IObfsCollection>& obfsCollection,
                 const std::shared_ptr<const OsmAnd::IRoadLocator> &roadLocator);
+        virtual ~ReverseGeocoder();
 
         virtual void performSearch(
             const ISearch::Criteria& criteria,
             const NewResultEntryCallback newResultEntryCallback,
                 const std::shared_ptr<const IQueryController>& queryController = nullptr) const;
-    private:
-        const std::shared_ptr<const IRoadLocator> roadLocator;
-
-        static bool DISTANCE_COMPARATOR(
-                const std::shared_ptr<const OsmAnd::ReverseGeocoder::ResultEntry> &a,
-                const std::shared_ptr<const OsmAnd::ReverseGeocoder::ResultEntry> &b);
-
-        AddressesByNameSearch* addressByNameSearch;
-        std::shared_ptr<const OsmAnd::ReverseGeocoder::ResultEntry> justifyResult(QVector<std::shared_ptr<const ResultEntry> > res) const;
-        QVector<std::shared_ptr<const ResultEntry> > justifyReverseGeocodingSearch(
-                const std::shared_ptr<const OsmAnd::ReverseGeocoder::ResultEntry> &road,
-                double knownMinBuildingDistance) const;
-        QVector<std::shared_ptr<const ResultEntry>> loadStreetBuildings(
-                const std::shared_ptr<const OsmAnd::ReverseGeocoder::ResultEntry> road,
-                const std::shared_ptr<const ResultEntry> street) const;
-        QVector<std::shared_ptr<const ResultEntry>> reverseGeocodeToRoads(
-                const LatLon searchPoint) const;
     };
 }
 
-#endif // REVERSEGEOCODER_H
+#endif // _OSMAND_CORE_REVERSE_GEOCODER_H_
