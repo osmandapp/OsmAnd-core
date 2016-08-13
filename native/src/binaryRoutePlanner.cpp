104e05cb185edcf3506f1e5527c5b10e1076df12
Victor Shcherb <victor.shcherb@gmail.com>
2016-08-13 18:16:54 +0200

    Fix #2599

--- native/src/binaryRoutePlanner.cpp
+++ native/src/binaryRoutePlanner.cpp
@@ -252,7 +252,9 @@ SHARED_PTR<RouteSegment> loadSameSegment(RoutingContext* ctx, SHARED_PTR<RouteSe
 SHARED_PTR<RouteSegment> initRouteSegment(RoutingContext* ctx, SHARED_PTR<RouteSegment> segment, bool positiveDirection) {
 	if(segment->getSegmentStart() == 0 && !positiveDirection && segment->getRoad()->getPointsLength() > 0) {
 		segment = loadSameSegment(ctx, segment, 1);
-	} else if(segment->getSegmentStart() == segment->getRoad()->getPointsLength() -1 && positiveDirection && segment->getSegmentStart() > 0) {
+	// } else if(segment->getSegmentStart() == segment->getRoad()->getPointsLength() -1 && positiveDirection && segment->getSegmentStart() > 0) {
+	// assymetric cause we calculate initial point differently (segmentStart means that point is between ]segmentStart-1, segmentStart]
+	} else if(segment->getSegmentStart() > 0 && positiveDirection) {
 		segment = loadSameSegment(ctx, segment, segment->getSegmentStart() -1);
 	}
 	if(segment.get() == NULL) {
