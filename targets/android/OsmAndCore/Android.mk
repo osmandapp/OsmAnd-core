fa34e49d60376c28a3ab49a8b010bdacf49fad9e
Victor Shcherb <victor.shcherb@gmail.com>
2016-09-08 00:36:35 +0200

    Update methods

--- targets/android/OsmAndCore/Android.mk
+++ targets/android/OsmAndCore/Android.mk
@@ -35,7 +35,6 @@ LOCAL_SRC_FILES := \
 	src/Logging.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/ElapsedTimer.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/common.cpp \
-	$(OSMAND_CORE_RELATIVE)/src/mapObjects.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/multipolygons.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/renderRules.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/rendering.cpp \
