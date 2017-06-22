66ea2a7326b1faa5dbc0ae3ab6504df0b72259ce
Alexey Kulish <alexey.kulish@osmand.net>
2017-06-22 10:42:09 +0300

    Modified Android.mk

--- targets/android/OsmAndCore/Android.mk
+++ targets/android/OsmAndCore/Android.mk
@@ -38,8 +38,11 @@ LOCAL_SRC_FILES := \
 	$(OSMAND_CORE_RELATIVE)/src/multipolygons.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/renderRules.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/rendering.cpp \
+	$(OSMAND_CORE_RELATIVE)/src/precalculatedRouteDirection.cpp \
+	$(OSMAND_CORE_RELATIVE)/src/routingConfiguration.cpp \
+	$(OSMAND_CORE_RELATIVE)/src/routingContext.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/binaryRead.cpp \
-        $(OSMAND_CORE_RELATIVE)/src/generalRouter.cpp \
+	$(OSMAND_CORE_RELATIVE)/src/generalRouter.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/binaryRoutePlanner.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/proto/osmand_index.pb.cc \
 	$(OSMAND_CORE_RELATIVE)/src/java_wrap.cpp
