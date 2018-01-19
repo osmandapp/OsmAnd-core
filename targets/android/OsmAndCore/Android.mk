b0251c69b60cf448c9f64dfe89b58ddf90d353e6
Alexey Kulish <alexey.kulish@osmand.net>
2018-01-19 13:22:15 +0300

    Fix build

--- targets/android/OsmAndCore/Android.mk
+++ targets/android/OsmAndCore/Android.mk
@@ -44,6 +44,7 @@ LOCAL_SRC_FILES := \
 	$(OSMAND_CORE_RELATIVE)/src/binaryRead.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/generalRouter.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/binaryRoutePlanner.cpp \
+	$(OSMAND_CORE_RELATIVE)/src/routeTypeRule.cpp \
 	$(OSMAND_CORE_RELATIVE)/src/proto/osmand_index.pb.cc \
 	$(OSMAND_CORE_RELATIVE)/src/java_wrap.cpp
 	
