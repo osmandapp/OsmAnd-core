a18f7d89aea6adf1a830d0fec3ddcb07602da92f
crimean <alexey.kulish@osmand.net>
2018-08-13 19:14:32 +0300

    Added -stdlib=libc++

--- externals/qtbase-android/build.sh
+++ externals/qtbase-android/build.sh
@@ -129,7 +129,7 @@ echo "Using toolchain '${TOOLCHAIN_PATH}'"
 
 # Prepare configuration
 QTBASE_CONFIGURATION=$(echo "
-	-release -opensource -confirm-license -c++std c++11 -no-accessibility -qt-sql-sqlite
+	-release -opensource -confirm-license -c++std c++11 -stdlib=libc++ -no-accessibility -qt-sql-sqlite
 	-no-qml-debug -qt-zlib -no-gif -no-libpng -no-libjpeg -no-openssl -qt-pcre
 	-nomake examples -nomake tools -no-gui -no-widgets -no-cups -no-iconv -no-icu -no-dbus
 	-no-opengl -no-evdev -qt-xcb
