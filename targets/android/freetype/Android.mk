980a4a57ea1c64df77098bc5deab9d1dbe7fd234
Alexey Kulish <alexey.kulish@osmand.net>
2018-08-16 16:34:15 +0300

    FreeType updated to 2.9.1

--- targets/android/freetype/Android.mk
+++ targets/android/freetype/Android.mk
@@ -20,7 +20,6 @@ LOCAL_SRC_FILES:= \
 	$(OSMAND_FREETYPE_RELATIVE)/src/base/ftstroke.c \
 	$(OSMAND_FREETYPE_RELATIVE)/src/base/ftsystem.c \
 	$(OSMAND_FREETYPE_RELATIVE)/src/base/fttype1.c \
-	$(OSMAND_FREETYPE_RELATIVE)/src/base/ftxf86.c \
 	$(OSMAND_FREETYPE_RELATIVE)/src/autofit/autofit.c \
 	$(OSMAND_FREETYPE_RELATIVE)/src/bdf/bdf.c \
 	$(OSMAND_FREETYPE_RELATIVE)/src/cff/cff.c \
