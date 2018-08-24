a5b6c5f97d273e199393a474f7a3a7a1546853f2
crimean <alexey.kulish@osmand.net>
2018-08-24 17:29:21 +0300

    Fix build

---
+++ externals/jpeg/jconfigint.h
@@ -0,0 +1,25 @@
+/* libjpeg-turbo build number */
+#define BUILD  ""
+
+/* Compiler's inline keyword */
+#undef inline
+
+/* How to obtain function inlining. */
+// "inline __attribute__((always_inline))"
+#define INLINE  
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME  "osmand_jpeg"
+
+/* Version number of package */
+#define VERSION  "1.0"
+
+/* The size of `size_t', as computed by sizeof. */
+/* #define SIZEOF_SIZE_T  */
+
+/* Define if your compiler has __builtin_ctzl() and sizeof(unsigned long) == sizeof(size_t). */
+/* #undef HAVE_BUILTIN_CTZL */
+
+/* Define to 1 if you have the <intrin.h> header file. */
+/* #undef HAVE_INTRIN_H */
+
