df4131892053e82233b85302ecd2659685de5766
Victor Shcherb <victor.shcherb@gmail.com>
2018-08-27 00:03:31 +0200

    Fix compilation linux

--- targets/.cmake/targets/amd64-linux-gcc.cmake
+++ targets/.cmake/targets/amd64-linux-gcc.cmake
@@ -3,6 +3,6 @@ set(CMAKE_TARGET_CPU_ARCH amd64)
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
 set(CMAKE_COMPILER_FAMILY gcc)
-set(CMAKE_C_COMPILER_FLAGS "-m64 -fstack-protector-all -Wstack-protector")
-set(CMAKE_CXX_COMPILER_FLAGS "-std=c++0x -m64 -fstack-protector-all -Wstack-protector")
+set(CMAKE_C_COMPILER_FLAGS "-m64 -msse3 -fstack-protector-all -Wstack-protector")
+set(CMAKE_CXX_COMPILER_FLAGS "-std=c++0x -m64 -msse3 -fstack-protector-all -Wstack-protector")
  
