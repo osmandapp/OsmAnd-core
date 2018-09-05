8f7b4ed5ec74ce3337546bf1752d0b844c2194ac
crimean <alexey.kulish@osmand.net>
2018-09-05 12:34:19 +0300

    Revert "Upgrade qtbase for ios to 5.11.1"
    This reverts commit d8620dcad60529d1eecf53d9aca6dff726718901.

--- externals/qtbase-ios/build.sh
+++ externals/qtbase-ios/build.sh
@@ -16,9 +16,9 @@ targetOS=$1
 compiler=$2
 
 QTBASE_CONFIGURATION=$(echo "
-	-release -opensource -confirm-license -c++std c++11 -static -no-accessibility -sql-sqlite -qt-sqlite
-	-qt-zlib -no-gif -no-libpng -no-libjpeg -no-openssl -qt-pcre
-	-nomake tests -nomake examples -nomake tools -no-gui -no-widgets -no-cups -no-iconv -no-icu -no-dbus
+	-release -opensource -confirm-license -c++std c++11 -static -largefile -no-accessibility -qt-sql-sqlite
+	-no-qml-debug -qt-zlib -no-gif -no-libpng -no-libjpeg -no-openssl -qt-pcre
+	-nomake examples -nomake tools -no-gui -no-widgets -no-cups -no-iconv -no-icu -no-dbus
 	-no-xcb -no-eglfs -no-directfb -no-linuxfb -no-kms -no-opengl -no-glib
 	-v
 " | tr '\n' ' ')
@@ -92,7 +92,7 @@ if [[ "$targetOS" == "ios" ]]; then
 			ln -s "../../upstream.patched.ios.simulator.${compiler}.static/lib/cmake" "cmake")
 
 		# Make universal libraries using lipo
-		libraries=(qtpcre2 Qt5Core Qt5Concurrent Qt5Network Qt5Sql Qt5Xml)
+		libraries=(qtpcre Qt5Core Qt5Concurrent Qt5Network Qt5Sql Qt5Xml)
 		for libName in "${libraries[@]}" ; do
 			echo "Packing '$libName'..."
 			lipo -create \
