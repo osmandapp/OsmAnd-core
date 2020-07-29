aa356ceae394980ed9f6d1b9e53e95a3a31a1689
PaulStets <stetsenko.pavlo@gmail.com>
2020-07-29 15:13:02 +0300

    Added archive writer

---
+++ include/OsmAndCore/ArchiveWriter.h
@@ -0,0 +1,33 @@
+#ifndef _OSMAND_CORE_ARCHIVE_READER_H_
+#define _OSMAND_CORE_ARCHIVE_READER_H_
+
+#include <OsmAndCore/stdlib_common.h>
+
+#include <OsmAndCore/QtExtensions.h>
+
+#include <QString>
+#include <QList>
+#include <QDateTime>
+
+#include <OsmAndCore.h>
+#include <OsmAndCore/PrivateImplementation.h>
+
+namespace OsmAnd
+{
+    class ArchiveWriter_P;
+    class OSMAND_CORE_API ArchiveWriter
+    {
+        Q_DISABLE_COPY_AND_MOVE(ArchiveWriter);
+    public:
+    private:
+        PrivateImplementation<ArchiveWriter_P> _p;
+    protected:
+    public:
+        ArchiveWriter();
+        virtual ~ArchiveWriter();
+
+        void createArchive(bool* const ok_, const QString& filePath, const QList<QString>& filesToArcive);
+    };
+}
+
+#endif // !defined(_OSMAND_CORE_ARCHIVE_READER_H_)
---
+++ src/ArchiveWriter_P.h
@@ -0,0 +1,39 @@
+#ifndef _OSMAND_CORE_ARCHIVE_READER_P_H_
+#define _OSMAND_CORE_ARCHIVE_READER_P_H_
+
+#include "stdlib_common.h"
+#include <functional>
+
+#include "QtExtensions.h"
+#include <QString>
+#include <QList>
+#include <QIODevice>
+
+#include <libarchive/archive.h>
+
+#include "OsmAndCore.h"
+#include "PrivateImplementation.h"
+#include "ArchiveWriter.h"
+
+namespace OsmAnd
+{
+    class ArchiveWriter;
+    class ArchiveWriter_P Q_DECL_FINAL
+    {
+        Q_DISABLE_COPY_AND_MOVE(ArchiveWriter_P);
+    public:
+
+    private:
+    protected:
+        ArchiveWriter_P(ArchiveWriter* const owner);
+    public:
+        virtual ~ArchiveWriter_P();
+
+        ImplementationInterface<ArchiveWriter> owner;
+        void createArchive(bool* const ok_, const QString& filePath, const QList<QString>& filesToArcive);
+
+    friend class OsmAnd::ArchiveWriter;
+    };
+}
+
+#endif // !defined(_OSMAND_CORE_ARCHIVE_READER_P_H_)
