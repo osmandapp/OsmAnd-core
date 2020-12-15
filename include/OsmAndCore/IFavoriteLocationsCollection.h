fccf3db3df22a92eb0a9790f02a46802694bc955
PaulStets <stetsenko.pavlo@gmail.com>
2020-12-15 11:55:15 +0200

    Refactor methods with description

--- include/OsmAndCore/IFavoriteLocationsCollection.h
+++ include/OsmAndCore/IFavoriteLocationsCollection.h
@@ -33,6 +33,16 @@ namespace OsmAnd
         virtual std::shared_ptr<IFavoriteLocation> createFavoriteLocation(
             const PointI position31,
             const QString& title = QString::null,
+            const QString& group = QString::null,
+            const ColorRGB color = ColorRGB()) = 0;
+        virtual std::shared_ptr<IFavoriteLocation> createFavoriteLocation(
+            const LatLon latLon,
+            const QString& title = QString::null,
+            const QString& group = QString::null,
+            const ColorRGB color = ColorRGB()) = 0;
+        virtual std::shared_ptr<IFavoriteLocation> createFavoriteLocation(
+            const PointI position31,
+            const QString& title = QString::null,
             const QString& description = QString::null,
             const QString& group = QString::null,
             const ColorRGB color = ColorRGB()) = 0;
