7341ff19f03afc24d7a7b59a973ce864db71e24a
PaulStets <stetsenko.pavlo@gmail.com>
2021-03-24 13:52:44 +0200

    Fix formatting

--- native/src/binaryRead.h
+++ native/src/binaryRead.h
@@ -442,14 +442,14 @@ struct RouteDataObject {
 	inline int getPointsLength() {
 		return (int)pointsX.size();
 	}
-    
-    std::vector<uint32_t> getPointTypes(int ind) {
-        if (ind >= pointTypes.size()) {
-            return {};
-        }
-        return pointTypes[ind];
-    }
-
+	
+	std::vector<uint32_t> getPointTypes(int ind) {
+		if (ind >= pointTypes.size()) {
+			return {};
+		}
+		return pointTypes[ind];
+	}
+	
 	bool loop() {
 		return pointsX[0] == pointsX[pointsX.size() - 1] && pointsY[0] == pointsY[pointsY.size() - 1];
 	}
