37cc425a37d6e10de6a28d4bcb3696ad99baaf39
Victor Shcherb <victor.shcherb@gmail.com>
2021-08-28 12:10:05 +0200

    Temporarily disable to speed up build

--- wrappers/android/build.sh
+++ wrappers/android/build.sh
@@ -61,21 +61,21 @@ if [[ $retcode -ne 0 ]]; then
 	exit $retcode
 fi
 
-buildArch "arm64-v8a"
+#buildArch "arm64-v8a"
 retcode=$?
 if [[ $retcode -ne 0 ]]; then
 	echo "buildArch(arm64-v8a) failed with $retcode, exiting..."
 	exit $retcode
 fi
 
-buildArch "x86"
+#buildArch "x86"
 retcode=$?
 if [[ $retcode -ne 0 ]]; then
 	echo "buildArch(x86) failed with $retcode, exiting..."
 	exit $retcode
 fi
 
-buildArch "x86_64"
+#buildArch "x86_64"
 retcode=$?
 if [[ $retcode -ne 0 ]]; then
         echo "buildArch(x86_64) failed with $retcode, exiting..."
