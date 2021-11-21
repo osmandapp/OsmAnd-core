ef930faa596281d01d5a132223440898d9f2f8ca
Alexey Pelykh <alexey.pelykh@gmail.com>
2021-11-21 19:31:16 +0100

    Support C++17 (except gdal and icu4c)

--- src/GlobalMemoryManagerOverride.cpp
+++ src/GlobalMemoryManagerOverride.cpp
@@ -2,7 +2,7 @@
 
 #include <QtGlobal>
 
-void* operator new(std::size_t count) throw(std::bad_alloc)
+void* operator new(std::size_t count) Q_DECL_NOEXCEPT_EXPR(false)
 {
     const auto ptr = OsmAnd::getMemoryManager()->allocate(count, "global");
     if (!ptr)
@@ -10,7 +10,7 @@ void* operator new(std::size_t count) throw(std::bad_alloc)
     return ptr;
 }
 
-void* operator new[](std::size_t count) throw(std::bad_alloc)
+void* operator new[](std::size_t count) Q_DECL_NOEXCEPT_EXPR(false)
 {
     const auto ptr = OsmAnd::getMemoryManager()->allocate(count, "global");
     if (!ptr)
@@ -18,35 +18,35 @@ void* operator new[](std::size_t count) throw(std::bad_alloc)
     return ptr;
 }
 
-void* operator new(std::size_t count, const std::nothrow_t& tag) Q_DECL_NOTHROW
+void* operator new(std::size_t count, const std::nothrow_t& tag) Q_DECL_NOEXCEPT
 {
     Q_UNUSED(tag);
     return OsmAnd::getMemoryManager()->allocate(count, "global");
 }
 
-void* operator new[](std::size_t count, const std::nothrow_t& tag) Q_DECL_NOTHROW
+void* operator new[](std::size_t count, const std::nothrow_t& tag) Q_DECL_NOEXCEPT
 {
     Q_UNUSED(tag);
     return OsmAnd::getMemoryManager()->allocate(count, "global");
 }
 
-void operator delete(void* ptr) Q_DECL_NOTHROW
+void operator delete(void* ptr) Q_DECL_NOEXCEPT
 {
     OsmAnd::getMemoryManager()->free(ptr, "global");
 }
 
-void operator delete[](void* ptr) Q_DECL_NOTHROW
+void operator delete[](void* ptr) Q_DECL_NOEXCEPT
 {
     OsmAnd::getMemoryManager()->free(ptr, "global");
 }
 
-void operator delete(void* ptr, const std::nothrow_t& tag) Q_DECL_NOTHROW
+void operator delete(void* ptr, const std::nothrow_t& tag) Q_DECL_NOEXCEPT
 {
     Q_UNUSED(tag);
     OsmAnd::getMemoryManager()->free(ptr, "global");
 }
 
-void operator delete[](void* ptr, const std::nothrow_t& tag) Q_DECL_NOTHROW
+void operator delete[](void* ptr, const std::nothrow_t& tag) Q_DECL_NOEXCEPT
 {
     Q_UNUSED(tag);
     OsmAnd::getMemoryManager()->free(ptr, "global");
