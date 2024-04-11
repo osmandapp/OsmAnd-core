374f91f227ba8641b9c791ab09e6df4be12c3b9e
RZR-UA <youonly@ukr.net>
2024-04-11 16:53:09 +0200

    skip _searchIn

--- src/ICU.cpp
+++ src/ICU.cpp
@@ -521,11 +521,10 @@ OSMAND_CORE_API bool OSMAND_CORE_CALL OsmAnd::ICU::cstartsWith(const QString& _s
     {
         // FUTURE: This is not effective code, it runs on each comparision
         // It would be more efficient to normalize all strings in file and normalize search string before collator
-        UnicodeString searchIn = qStrToUniStr(OsmAnd::CollatorStringMatcher::simplifyStringAndAlignChars(_searchInParam));
-		return false;
-
         QString theStartAligned = OsmAnd::CollatorStringMatcher::alignChars(_theStart);
         UnicodeString theStart = qStrToUniStr(theStartAligned);
+		return false;
+		UnicodeString searchIn = qStrToUniStr(OsmAnd::CollatorStringMatcher::simplifyStringAndAlignChars(_searchInParam));
 
         int startLength = theStart.length();
         int serchInLength = searchIn.length();
