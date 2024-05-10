da0b149a671078227a594e82a34113b0a898b27d
lightyear15 <g.minist8@gmail.com>
2024-05-10 09:50:38 +0200

    split OsmAndCore.h in OsmAndCoreAPI.h
    Separate definition of OSMAND_CORE_API macro into its own header file

--- include/OsmAndCore.h
+++ include/OsmAndCore.h
@@ -1,51 +1,7 @@
 #ifndef _OSMAND_CORE_OSMAND_CORE_H_
 #define _OSMAND_CORE_OSMAND_CORE_H_
 
-#if defined(OSMAND_CORE_EXPORTS)
-#   if defined(_WIN32) || defined(__CYGWIN__)
-#       define OSMAND_CORE_API \
-            __declspec(dllexport)
-#       define OSMAND_CORE_CALL \
-            __stdcall
-#   else
-#       if __GNUC__ >= 4
-#           define OSMAND_CORE_API \
-                __attribute__ ((visibility ("default")))
-#       else
-#           define OSMAND_CORE_API
-#       endif
-#       define OSMAND_CORE_CALL
-#   endif
-#elif !defined(OSMAND_CORE_STATIC)
-#   if defined(_WIN32) || defined(__CYGWIN__)
-#       define OSMAND_CORE_API \
-            __declspec(dllimport)
-#       define OSMAND_CORE_API_FUNCTION \
-            extern OSMAND_CORE_API
-#       define OSMAND_CORE_CALL \
-            __stdcall
-#   else
-#       if __GNUC__ >= 4
-#           define OSMAND_CORE_API \
-                __attribute__ ((visibility ("default")))
-#       else
-#           define OSMAND_CORE_API
-#       endif
-#       define OSMAND_CORE_CALL
-#   endif
-#else
-#   define OSMAND_CORE_API
-#   define OSMAND_CORE_CALL
-#endif
-
-#if !defined(OSMAND_DEBUG)
-#   if defined(DEBUG) || defined(_DEBUG)
-#       define OSMAND_DEBUG 1
-#   endif
-#endif
-#if !defined(OSMAND_DEBUG)
-#   define OSMAND_DEBUG 0
-#endif
+#include "OsmAndCoreAPI.h"
 
 #include <memory>
 
@@ -66,7 +22,7 @@ namespace OsmAnd
     class IMemoryManager;
 
     OSMAND_CORE_API bool OSMAND_CORE_CALL InitializeCore(
-        const std::shared_ptr<const ICoreResourcesProvider>& coreResourcesProvider, 
+        const std::shared_ptr<const ICoreResourcesProvider>& coreResourcesProvider,
         const char* appFontsPath = nullptr);
     OSMAND_CORE_API const std::shared_ptr<const ICoreResourcesProvider>& OSMAND_CORE_CALL getCoreResourcesProvider();
     OSMAND_CORE_API const QString& OSMAND_CORE_CALL getFontDirectory();
