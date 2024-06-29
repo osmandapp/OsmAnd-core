623e2df94f339b23f0d6198f04d491ffad4eeb2b
alex-osm <alex.morozov.osm@gmail.com>
2024-06-29 18:45:38 +0300

    Test gradle interpolation (near)

--- src/Map/GeoTileRasterizer_P.cpp
+++ src/Map/GeoTileRasterizer_P.cpp
@@ -231,7 +231,7 @@ QHash<OsmAnd::BandIndex, sk_sp<const SkImage>> OsmAnd::GeoTileRasterizer_P::rast
             "-co", "SPARSE_OK=TRUE",
             //"-t_srs", "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs",
             "-t_srs", "epsg:3857",
-            "-r", "cubic",
+            "-r", "near",
             "-multi",
             //"-wo", "NUM_THREADS=ALL_CPUS",
             "-wo", "NUM_THREADS=2",
