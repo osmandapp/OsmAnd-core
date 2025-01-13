84b8b1a0f8533f8b35ce8c373b61295ad3200213
vshcherb <victor.shcherb+git@gmail.com>
2025-01-13 11:43:39 +0200

    Preview for #3959 - filter buildings

--- externals/qtbase-ios/link_qt_for_arm_device.sh
+++ externals/qtbase-ios/link_qt_for_arm_device.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rm upstream.patched.ios.clang || true
 ln -s upstream.patched.ios.device.clang.static upstream.patched.ios.clang 
+rm upstream.patched.ios.clang-fat.static || true
+ln -s upstream.patched.ios.device.clang.static upstream.patched.ios.clang-fat.static
