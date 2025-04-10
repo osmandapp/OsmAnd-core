22fee0133966053d1cb441a927a1216cab163d9e
Ivan Pyrohivskyi <ivan@Mac-mini-Ivan.local>
2025-04-10 18:43:19 +0300

    ArabicNormalizer expand

--- src/ArabicNormalizer.cpp
+++ src/ArabicNormalizer.cpp
@@ -1,10 +1,28 @@
 #include "ArabicNormalizer.h"
+#include <QMap>
+#include <QRegularExpression>
 
 // Arabic-specific character ranges
-static const QString ARABIC_DIACRITICS = QString::fromUtf16(u"\u064B\u064C\u064D\u064E\u064F\u0650\u0651\u0652");
 static const QString ARABIC_DIGITS = QString::fromUtf16(u"٠١٢٣٤٥٦٧٨٩");
 static const QString DIGITS_REPLACEMENT = QStringLiteral("0123456789");
-static const QChar KASHIDA = QChar(0x0640);  // Arabic Tatweel (Kashida)
+
+static const QVector<QString> ARABIC_DIACRITIC_REGEX = {
+    QString::fromUtf8("[\u064B-\u065F]"),
+    QString::fromUtf8("[\u0610-\u061A]"),
+    QString::fromUtf8("[\u06D6-\u06ED]"),
+    QString::fromUtf8("\u0640"),
+    QString::fromUtf8("\u0670")
+};
+
+static const QMap<QString, QString> ARABIC_DIACRITIC_REPLACE = {
+    {QString::fromUtf8("\u0624"), QString::fromUtf8("\u0648")}, // Replace Waw Hamza Above by Waw
+    {QString::fromUtf8("\u0629"), QString::fromUtf8("\u0647")}, // Replace Ta Marbuta by Ha
+    {QString::fromUtf8("\u064A"), QString::fromUtf8("\u0649")}, // Replace Ya by Alif Maksura
+    {QString::fromUtf8("\u0626"), QString::fromUtf8("\u0649")}, // Replace Ya Hamza Above by Alif Maksura
+    {QString::fromUtf8("\u0622"), QString::fromUtf8("\u0627")}, // Replace Alifs with Hamza Above
+    {QString::fromUtf8("\u0623"), QString::fromUtf8("\u0627")}, // Replace Alifs with Hamza Below
+    {QString::fromUtf8("\u0625"), QString::fromUtf8("\u0627")}  // Replace with Madda Above by Alif
+};
 
 bool OsmAnd::ArabicNormalizer::isSpecialArabic(const QString& text) {
     if (text.isEmpty()) {
@@ -14,7 +32,7 @@ bool OsmAnd::ArabicNormalizer::isSpecialArabic(const QString& text) {
     QChar firstChar = text.at(0);
     if (isArabicCharacter(firstChar)) {
         for (const QChar& c : text) {
-            if (isDiacritic(c) || isArabicDigit(c) || isKashida(c)) {
+            if (isDiacritic(c) || isArabicDigit(c) || isNeedReplace(c)) {
                 return true;
             }
         }
@@ -29,15 +47,18 @@ QString OsmAnd::ArabicNormalizer::normalize(const QString& text) {
     }
 
     QString result = text;
-    
-    // Remove diacritics
-    for (const QChar& diacritic : ARABIC_DIACRITICS) {
-        result.remove(diacritic);
+
+    // Remove diacritics using regex
+    for (const QString& regexStr : ARABIC_DIACRITIC_REGEX) {
+        QRegularExpression regex(regexStr);
+        result.replace(regex, "");
     }
 
-    // Remove Kashida
-    result.remove(KASHIDA);
-
+    // Replace characters
+    QMap<QString, QString>::const_iterator it;
+    for (it = ARABIC_DIACRITIC_REPLACE.constBegin(); it != ARABIC_DIACRITIC_REPLACE.constEnd(); ++it) {
+        result.replace(it.key(), it.value());
+    }
     return replaceDigits(result);
 }
 
@@ -60,17 +81,20 @@ QString OsmAnd::ArabicNormalizer::replaceDigits(const QString& text) {
 }
 
 bool OsmAnd::ArabicNormalizer::isDiacritic(QChar c) {
-    return ARABIC_DIACRITICS.contains(c);
+    return (c >= QChar(0x064B) && c <= QChar(0x065F)) ||
+           (c >= QChar(0x0610) && c <= QChar(0x061A)) ||
+           (c >= QChar(0x06D6) && c <= QChar(0x06ED)) ||
+           c == QChar(0x0640) || c == QChar(0x0670);
 }
 
 bool OsmAnd::ArabicNormalizer::isArabicDigit(QChar c) {
     return (c.unicode() >= 0x0660 && c.unicode() <= 0x0669);// Arabic-Indic digits
 }
 
-bool OsmAnd::ArabicNormalizer::isKashida(QChar c) {
-    return (c == KASHIDA);
-}
-
 bool OsmAnd::ArabicNormalizer::isArabicCharacter(QChar c) {
     return (c.unicode() >= 0x0600 && c.unicode() <= 0x06FF);  // Arabic Unicode block
+}
+
+bool OsmAnd::ArabicNormalizer::isNeedReplace(QChar c) {
+    return ARABIC_DIACRITIC_REPLACE.contains(QString(c));
 }
