ef4a22b18cec6fcc756a59f0432ca867fa22b87e
vshcherb <victor.shcherb@gmail.com>
2025-08-10 10:55:56 +0300

    Revert and try begin/end

--- include/OsmAndCore/stdlib_common.h
+++ include/OsmAndCore/stdlib_common.h
@@ -15,27 +15,30 @@ namespace std
     template<class CONTAINER_TYPE, class ITEM_TYPE>
     inline auto find(CONTAINER_TYPE& container, const ITEM_TYPE& itemValue) -> decltype(begin(container))
     {
+        // return find(std::begin(container), std::end(container), itemValue);
         return find(std::begin(container), std::end(container), itemValue);
     }
 
     template<class CONTAINER_TYPE, class ITEM_TYPE>
     inline bool contains(const CONTAINER_TYPE& container, const ITEM_TYPE& itemValue)
     {
-        const auto itEnd = std::end(container);
-        return find(std::begin(container), std::end(container), itemValue) != itEnd;
+        const auto itEnd = container.end();
+        return find(container.begin(), container.end(), itemValue) != itEnd;
     }
 
     template<class CONTAINER_TYPE, class PREDICATE>
     inline auto find_if(CONTAINER_TYPE& container, PREDICATE predicate) -> decltype(begin(container))
     {
-        return find_if(std::begin(container), std::end(container), predicate);
+        // return find_if(std::begin(container), std::end(container), predicate);
+        return find_if(std::begin(container), container.end(), predicate);
     }
 
     template<class CONTAINER_TYPE, class PREDICATE>
     inline bool contains_if(const CONTAINER_TYPE& container, PREDICATE predicate)
     {
-        const auto itEnd = std::end(container);
-        return find_if(std::begin(container), std::end(container), predicate) != itEnd;
+        const auto itEnd = container.end();
+        // return find_if(std::begin(container), std::end(container), predicate) != itEnd;
+        return find_if(container.begin(), container.end(), predicate) != itEnd;
     }
 
     template<class CONTAINER_TYPE, class PREDICATE>
@@ -48,27 +51,29 @@ namespace std
     template<class CONTAINER_TYPE, class T>
     inline T accumulate(const CONTAINER_TYPE& container, T initialValue)
     {
-        return accumulate(std::begin(container), std::end(container), initialValue);
+        // return accumulate(std::begin(container), std::end(container), initialValue);
+        return accumulate(container.begin(), container.end(), initialValue);
     }
 
     template<class CONTAINER_TYPE, class T, class OPERATION>
     inline T accumulate(const CONTAINER_TYPE& container, T initialValue, OPERATION operation)
     {
-        return accumulate(std::begin(container), std::end(container), initialValue, operation);
+        // return accumulate(std::begin(container), std::end(container), initialValue, operation);
+        return accumulate(container.begin(), container.end(), initialValue, operation);
     }
 
     template<class CONTAINER_TYPE>
     inline void sort(CONTAINER_TYPE& container)
     {
-     //   return sort(std::begin(container), std::end(container));
-	return sort(container.begin(), container.end());
+        // return sort(std::begin(container), std::end(container));
+        return sort(container.begin(), container.end());
     }
 
     template<class CONTAINER_TYPE, class PREDICATE>
     inline void sort(CONTAINER_TYPE& container, PREDICATE predicate)
     {
-     //   return sort(std::begin(container), std::end(container), predicate);
-	return sort(container.begin(), container.end(), predicate);
+        // return sort(std::begin(container), std::end(container), predicate);
+        return sort(container.begin(), container.end(), predicate);
     }
 }
 
