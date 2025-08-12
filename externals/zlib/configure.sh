452d74f2d1ff1e3cfa04a5f8ae78259b87e48c06
RZR-UA <90444451+RZR-UA@users.noreply.github.com>
2025-08-12 17:37:08 +0300

    Update links from creator to builder (#895)

--- externals/zlib/configure.sh
+++ externals/zlib/configure.sh
@@ -8,5 +8,5 @@ fi
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SRCLOC/../../../build/utils/functions.sh"
 
-prepareUpstreamFromTarArchive "$SRCLOC" "http://creator.osmand.net/dependencies-mirror/zlib-1.2.11.tar.gz"
+prepareUpstreamFromTarArchive "$SRCLOC" "http://builder.osmand.net/dependencies-mirror/zlib-1.2.11.tar.gz"
 patchUpstream "$SRCLOC"
