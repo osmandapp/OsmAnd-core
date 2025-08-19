b183bd01d9cdd4f6d70d64d328f23bccaa3db7be
Aleksandr Panchenko <alex.panchenko.osmand@gmail.com>
2025-08-19 20:52:23 +0300

    fix prepareUpstreamFromGit

--- externals/qtbase-android/configure.sh
+++ externals/qtbase-android/configure.sh
@@ -8,5 +8,5 @@ fi
 SRCLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SRCLOC/../../../build/utils/functions.sh"
 
-prepareUpstreamFromGit "$SRCLOC" "https://github.com/qt/qtbase" "v5.15.4-lts-lgpl"
+prepareUpstreamFromGit "$SRCLOC" "https://github.com/osmandapp/OsmAnd-external-qtbase.git" "v5.15.4"
 patchUpstream "$SRCLOC"
