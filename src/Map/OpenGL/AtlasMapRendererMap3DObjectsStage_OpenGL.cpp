602e0cece28db56a93f2a1ec4b8c7739eed1ad51
Alex Andrewschenko <andrewschenko@gmail.com>
2026-07-15 13:48:20 +0200

    Fix crash during 3D buildings rendering (#1081)

--- src/Map/OpenGL/AtlasMapRendererMap3DObjectsStage_OpenGL.cpp
+++ src/Map/OpenGL/AtlasMapRendererMap3DObjectsStage_OpenGL.cpp
@@ -711,6 +711,7 @@ void AtlasMapRendererMap3DObjectsStage_OpenGL::getResourcesInGPU(
                 }
                 if (allPresent)
                 {
+                    pUnderscaledTileIdN = underscaledTileIdsN.constData();
                     for (auto tileIdx = 0; tileIdx < subtilesCount; tileIdx++)
                     {
                         const auto& underscaledTileId = *(pUnderscaledTileIdN++);
